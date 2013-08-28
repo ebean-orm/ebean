236fb5541bfbf8e58a171ddc0cbf6c94a93b99b3
Christian Romming <christian@datadanze>
2013-08-28 23:15:20 +0200

    Fixed whitespace in test.

--- src/test/java/com/avaje/tests/basic/TestDeleteCascadingOneToMany.java
+++ src/test/java/com/avaje/tests/basic/TestDeleteCascadingOneToMany.java
@@ -10,12 +10,12 @@ public class TestDeleteCascadingOneToMany extends TestCase {
 
 	public void testDeleteCascadingOneToMany() {
 		Section s0 = new Section("some content");
-        Article a0 = new Article("art1","auth1");
-        a0.addSection(s0);
-        
-        Ebean.save(a0);
-        
-        Ebean.delete(a0);
+		Article a0 = new Article("art1","auth1");
+		a0.addSection(s0);
+
+		Ebean.save(a0);
+
+		Ebean.delete(a0);
 	}
 
 }
