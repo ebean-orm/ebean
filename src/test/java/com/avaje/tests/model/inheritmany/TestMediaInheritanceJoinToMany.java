5049694fd339fba7b30bb99487fd75418e81fa1c
Rob Bygrave <robin.bygrave@gmail.com>
2014-04-29 23:19:03 +1200

    Add test for old 408 bug - does not reproduce, not sure when fixed at this point

---
+++ src/test/java/com/avaje/tests/model/inheritmany/TestMediaInheritanceJoinToMany.java
@@ -0,0 +1,38 @@
+package com.avaje.tests.model.inheritmany;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.avaje.ebean.BaseTestCase;
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
+
+public class TestMediaInheritanceJoinToMany extends BaseTestCase {
+
+  @Test
+  public void test() {
+    
+    
+    String name = "nopic";
+    
+    MProfile profileWithNoPic = new MProfile();
+    profileWithNoPic.setName(name);
+    
+    Ebean.save(profileWithNoPic);
+    
+    Query<MProfile> query = Ebean.find(MProfile.class).fetch("picture").where().eq("name", name).query();
+    
+    // assert we get the profile with a null picture
+    MProfile profile = query.findUnique();
+    Assert.assertNotNull(profile);
+    
+    // select t0.id c0, t0.name c1, t1.type c2, t1.id c3, t1.url c4, t1.note c5 
+    // from profile t0 
+    // left outer join media t1 on t1.id = t0.picture_id and t1.type = 'Picture'  
+    // where t0.name = ? ; --bind(nopic)
+    
+    // specifically t1.type = 'Picture' ... on on the join and not in the where
+        
+  }
+  
+}
