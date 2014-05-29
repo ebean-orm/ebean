84e5a6145e288cad9283eee60810080e8483fb27
Rob Bygrave <robin.bygrave@gmail.com>
2014-05-30 01:35:19 +1200

    Fix for #137 - transaction.setBatchFlushOnQuery(false) not working for some queries

--- src/main/java/com/avaje/ebeaninternal/server/query/DefaultOrmQueryEngine.java
+++ src/main/java/com/avaje/ebeaninternal/server/query/DefaultOrmQueryEngine.java
@@ -19,112 +19,107 @@ import com.avaje.ebeaninternal.server.deploy.BeanDescriptorManager;
  */
 public class DefaultOrmQueryEngine implements OrmQueryEngine {
 
-    /**
-     * Find using predicates
-     */
-    private final CQueryEngine queryEngine;
-      
-    /**
-     * Create the Finder.
-     */
-    public DefaultOrmQueryEngine(BeanDescriptorManager descMgr, CQueryEngine queryEngine) {
-   
-        this.queryEngine = queryEngine;
+  /**
+   * Find using predicates
+   */
+  private final CQueryEngine queryEngine;
+
+  /**
+   * Create the Finder.
+   */
+  public DefaultOrmQueryEngine(BeanDescriptorManager descMgr, CQueryEngine queryEngine) {
+
+    this.queryEngine = queryEngine;
+  }
+
+  /**
+   * Flushes the jdbc batch by default unless explicitly turned off on the transaction.
+   */
+  private <T> void flushJdbcBatchOnQuery(OrmQueryRequest<T> request) {
+
+    SpiTransaction t = request.getTransaction();
+    if (t.isBatchFlushOnQuery()) {
+      // before we perform a query, we need to flush any
+      // previous persist requests that are queued/batched.
+      // The query may read data affected by those requests.
+      t.flushBatch();
     }
+  }
+
+  public <T> int findRowCount(OrmQueryRequest<T> request) {
+
+    flushJdbcBatchOnQuery(request);
+    return queryEngine.findRowCount(request);
+  }
+
+  public <T> BeanIdList findIds(OrmQueryRequest<T> request) {
+
+    flushJdbcBatchOnQuery(request);
+    return queryEngine.findIds(request);
+  }
+
+  public <T> QueryIterator<T> findIterate(OrmQueryRequest<T> request) {
+
+    // LIMITATION: You can not use QueryIterator to load bean cache
+
+    flushJdbcBatchOnQuery(request);
+    return queryEngine.findIterate(request);
+  }
+
+  public <T> BeanCollection<T> findMany(OrmQueryRequest<T> request) {
+
+    flushJdbcBatchOnQuery(request);
+
+    BeanFinder<T> finder = request.getBeanFinder();
+
+    BeanCollection<T> result;
+    if (finder != null) {
+      // this bean type has its own specific finder
+      result = finder.findMany(request);
+    } else {
+      result = queryEngine.findMany(request);
+    }
+
+    SpiQuery<T> query = request.getQuery();
     
-    public <T> int findRowCount(OrmQueryRequest<T> request){
-    	
-    	return queryEngine.findRowCount(request);
+    if (query.isLoadBeanCache()) {
+      // load the individual beans into the bean cache
+      BeanDescriptor<T> descriptor = request.getBeanDescriptor();
+      Collection<T> c = result.getActualDetails();
+      for (T bean : c) {
+        descriptor.cacheBeanPutData((EntityBean) bean);
+      }
     }
 
-    public <T> BeanIdList findIds(OrmQueryRequest<T> request){
-    	
-    	return queryEngine.findIds(request);
+    if (!result.isEmpty() && query.isUseQueryCache()) {
+      // load the query result into the query cache
+      request.putToQueryCache(result);
     }
 
+    return result;
+  }
 
-    public <T> QueryIterator<T> findIterate(OrmQueryRequest<T> request) {
+  /**
+   * Find a single bean using its unique id.
+   */
+  public <T> T findId(OrmQueryRequest<T> request) {
 
-        // LIMITATION: You can not use QueryIterator to load bean cache
- 
-        SpiTransaction t = request.getTransaction();
-        
-        // before we perform a query, we need to flush any
-        // previous persist requests that are queued/batched.
-        // The query may read data affected by those requests.
-        t.flushBatch();
-        
-        return queryEngine.findIterate(request);
-    }
-    
-	public <T> BeanCollection<T> findMany(OrmQueryRequest<T> request) {
+    flushJdbcBatchOnQuery(request);
 
-        SpiQuery<T> query = request.getQuery();
-		
-    	BeanCollection<T> result = null;
- 
-        SpiTransaction t = request.getTransaction();
-        
-        // before we perform a query, we need to flush any
-        // previous persist requests that are queued/batched.
-        // The query may read data affected by those requests.
-        t.flushBatch();
+    BeanFinder<T> finder = request.getBeanFinder();
 
-        BeanFinder<T> finder = request.getBeanFinder();
-        if (finder != null) {
-            // this bean type has its own specific finder
-            result = finder.findMany(request);
-        } else {
-        	result = queryEngine.findMany(request);
-        }
-
-    	if (query.isLoadBeanCache()){
-        	// load the individual beans into the bean cache
-        	BeanDescriptor<T> descriptor = request.getBeanDescriptor();
-        	Collection<T> c  = result.getActualDetails();
-        	for (T bean : c) {
-        		descriptor.cacheBeanPutData((EntityBean)bean);
-          }
-        }
-
-    	if (!result.isEmpty() && query.isUseQueryCache()){
-        	// load the query result into the query cache
-        	request.putToQueryCache(result);        		
-    	}
-        
-        return result;
+    T result;
+    if (finder != null) {
+      result = finder.find(request);
+    } else {
+      result = queryEngine.find(request);
     }
 
-
-    /**
-     * Find a single bean using its unique id.
-     */
-	public <T> T findId(OrmQueryRequest<T> request) {
-        
-		T result = null;
-		        
-        SpiTransaction t = request.getTransaction();
-        
-        if (t.isBatchFlushOnQuery()){
-            // before we perform a query, we need to flush any
-            // previous persist requests that are queued/batched.
-            // The query may read data affected by those requests.
-        	t.flushBatch();
-        }
-        
-        BeanFinder<T> finder = request.getBeanFinder();
-        if (finder != null) {
-            result =  finder.find(request);
-        } else {
-        	result = queryEngine.find(request);
-        }
-        
-        if (result != null && request.isUseBeanCache()){
-        	request.getBeanDescriptor().cacheBeanPutData((EntityBean)result);        		
-        }
-        
-        return result;
+    if (result != null && request.isUseBeanCache()) {
+      request.getBeanDescriptor().cacheBeanPutData((EntityBean) result);
     }
 
+    return result;
+  }
 
 }
