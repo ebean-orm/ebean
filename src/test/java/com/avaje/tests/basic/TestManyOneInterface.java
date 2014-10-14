1065a8470933b15c1379d8ec0012ef38a1797262
Eddie Mc Greal <e.mcgreal@imilia.com>
2014-10-14 08:10:31 +0200

    FIX for ManyToOne targetType & OSGI MANIFEST

---
+++ src/test/java/com/avaje/tests/basic/TestManyOneInterface.java
@@ -0,0 +1,32 @@
+package com.avaje.tests.basic;
+
+import org.junit.Test;
+
+import com.avaje.ebean.BaseTestCase;
+import com.avaje.ebean.Ebean;
+import com.avaje.tests.model.basic.ResetBasicData;
+import com.avaje.tests.model.interfaces.Address;
+import com.avaje.tests.model.interfaces.IAddress;
+import com.avaje.tests.model.interfaces.IPerson;
+import com.avaje.tests.model.interfaces.Person;
+
+public class TestManyOneInterface extends BaseTestCase {
+
+  @Test
+	public void test() {
+		
+		ResetBasicData.reset();
+		
+		IAddress a = new Address();
+		
+		IPerson p = new Person();
+		
+		p.setDefaultAddress(a);
+		
+		Ebean.save(a);
+		Ebean.save(p);
+		
+		//Assert.assertTrue();
+
+	}
+}
---
+++ src/test/java/com/avaje/tests/model/interfaces/IPerson.java
@@ -0,0 +1,6 @@
+package com.avaje.tests.model.interfaces;
+
+public interface IPerson {
+	public IAddress getDefaultAddress();
+	public void setDefaultAddress(IAddress address);
+}
