40f7a0088d5804203796651f8c45116eff1a018d
rbygrave <robin.bygrave@gmail.com>
2015-05-14 23:22:19 +1200

    No effective change - add tests for ConvertInetAddressesTest and DefaultTypeFactoryTest

--- src/test/java/com/avaje/ebeaninternal/server/type/DefaultTypeFactoryTest.java
+++ src/test/java/com/avaje/ebeaninternal/server/type/DefaultTypeFactoryTest.java
@@ -2,15 +2,26 @@ package com.avaje.ebeaninternal.server.type;
 
 import org.junit.Test;
 
+import java.sql.Types;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by rob on 14/05/15.
- */
+
 public class DefaultTypeFactoryTest {
+
+  DefaultTypeFactory defaultTypeFactory = new DefaultTypeFactory(null);
 
   @Test
   public void testCreateBoolean() throws Exception {
+
+    ScalarType<Boolean> stIntBoolean = defaultTypeFactory.createBoolean("0", "1");
+    assertEquals(Types.INTEGER, stIntBoolean.getJdbcType());
+
+    stIntBoolean = defaultTypeFactory.createBoolean("1", "2");
+    assertEquals(Types.INTEGER, stIntBoolean.getJdbcType());
+
+    ScalarType<Boolean> stStringBoolean = defaultTypeFactory.createBoolean("Y", "N");
+    assertEquals(Types.VARCHAR, stStringBoolean.getJdbcType());
 
   }
 }
