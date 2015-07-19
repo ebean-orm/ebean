1c040e4583efc53a1d6e32df8090c58cfbb4e619
Robin Bygrave <robin.bygrave@gmail.com>
2015-07-20 10:24:10 +1200

    No effective change - code format

--- src/main/java/com/avaje/ebeaninternal/server/core/SpiOrmQueryRequest.java
+++ src/main/java/com/avaje/ebeaninternal/server/core/SpiOrmQueryRequest.java
@@ -16,94 +16,89 @@ import java.util.Set;
  */
 public interface SpiOrmQueryRequest<T> {
 
-    /**
-     * Return the query.
-     */
-    public SpiQuery<T> getQuery();
+  /**
+   * Return the query.
+   */
+  SpiQuery<T> getQuery();
 
-    /**
-     * Return the associated BeanDescriptor.
-     */
-    public BeanDescriptor<?> getBeanDescriptor();
+  /**
+   * Return the associated BeanDescriptor.
+   */
+  BeanDescriptor<?> getBeanDescriptor();
 
-    /**
-     * This will create a local (readOnly) transaction if no current transaction
-     * exists.
-     * <p>
-     * A transaction may have been passed in explicitly or currently be active
-     * in the thread local. If not, then a readOnly transaction is created to
-     * execute this query.
-     * </p>
-     */
-    public void initTransIfRequired();
+  /**
+   * This will create a local (readOnly) transaction if no current transaction
+   * exists.
+   * <p>
+   * A transaction may have been passed in explicitly or currently be active
+   * in the thread local. If not, then a readOnly transaction is created to
+   * execute this query.
+   * </p>
+   */
+  void initTransIfRequired();
 
-    /**
-     * Will end a locally created transaction.
-     * <p>
-     * It ends the transaction by using a rollback() as the transaction is known
-     * to be readOnly.
-     * </p>
-     */
-    public void endTransIfRequired();
+  /**
+   * Will end a locally created transaction.
+   * <p>
+   * It ends the transaction by using a rollback() as the transaction is known
+   * to be readOnly.
+   * </p>
+   */
+  void endTransIfRequired();
 
-    /**
-     * Execute the query as findById.
-     */
-    public Object findId();
+  /**
+   * Execute the query as findById.
+   */
+  Object findId();
 
-    /**
-     * Execute the find row count query.
-     */
-    public int findRowCount();
+  /**
+   * Execute the find row count query.
+   */
+  int findRowCount();
 
-    /**
-     * Execute the find ids query.
-     */
-    public List<Object> findIds();
+  /**
+   * Execute the find ids query.
+   */
+  List<Object> findIds();
 
   /**
    * Execute the find returning a QueryIterator and visitor pattern.
    */
-  public void findEach(QueryEachConsumer<T> consumer);
+  void findEach(QueryEachConsumer<T> consumer);
 
   /**
    * Execute the find returning a QueryIterator and visitor pattern.
    */
-  public void findEachWhile(QueryEachWhileConsumer<T> consumer);
+  void findEachWhile(QueryEachWhileConsumer<T> consumer);
 
   /**
-     * Execute the find returning a QueryIterator.
-     */
-    public QueryIterator<T> findIterate();
-    
-    /**
-     * Execute the query as findList.
-     */
-    public List<T> findList();
+   * Execute the find returning a QueryIterator.
+   */
+  QueryIterator<T> findIterate();
 
-    /**
-     * Execute the query as findSet.
-     */
-    public Set<?> findSet();
+  /**
+   * Execute the query as findList.
+   */
+  List<T> findList();
 
-    /**
-     * Execute the query as findMap.
-     */
-    public Map<?, ?> findMap();
+  /**
+   * Execute the query as findSet.
+   */
+  Set<?> findSet();
 
-    /**
-     * Try to get the object out of the persistence context.
-     */
-    //public T getFromPersistenceContextOrCache();
+  /**
+   * Execute the query as findMap.
+   */
+  Map<?, ?> findMap();
 
-    /**
-     * Try to get the query result from the query cache.
-     */
-    public BeanCollection<T> getFromQueryCache();
+  /**
+   * Try to get the query result from the query cache.
+   */
+  BeanCollection<T> getFromQueryCache();
 
-    /**
-     * Return the Database platform like clause.
-     */
-    public String getDBLikeClause();
+  /**
+   * Return the Database platform like clause.
+   */
+  String getDBLikeClause();
 
 }
