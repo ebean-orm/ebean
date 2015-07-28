3549f882329d233abc7961580717b874fdaa3002
Kefeng Deng <dkf_genius@hotmail.com>
2015-07-28 21:31:49 +1200

    Introduced a service interface to allow multiple ClassPathSearchs with its own customized search way

--- src/main/java/com/avaje/ebeaninternal/server/core/BootupClassPathSearch.java
+++ src/main/java/com/avaje/ebeaninternal/server/core/BootupClassPathSearch.java
@@ -1,109 +1,127 @@
 package com.avaje.ebeaninternal.server.core;
 
-import com.avaje.ebeaninternal.server.util.ClassPathSearch;
+import com.avaje.ebeaninternal.api.ClassPathSearchService;
 import com.avaje.ebeaninternal.server.util.ClassPathSearchFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Searches for interesting classes such as Entities, Embedded and ScalarTypes.
  */
 public class BootupClassPathSearch {
 
-	private static final Logger logger = LoggerFactory.getLogger(BootupClassPathSearch.class);
+  private static final Logger logger = LoggerFactory.getLogger(BootupClassPathSearch.class);
 
-	private final Object monitor = new Object();
+  private final Object monitor = new Object();
 
-	private final ClassLoader classLoader;
+  private final ClassLoader classLoader;
 
-	private final List<String> packages;
+  private final List<String> packages;
 
   private final List<String> jars;
-	
-	private BootupClasses bootupClasses;
+
+  private List<ClassPathSearchService> classPathSearchServices;
+
+  private BootupClasses bootupClasses;
 
   private final String classPathReaderClassName;
 
-	/**
-	 * Construct and search for interesting classes.
-	 */
-	public BootupClassPathSearch(ClassLoader classLoader, List<String> packages, List<String> jars, String classPathReaderClassName) {
-		this.classLoader = (classLoader == null) ? getClass().getClassLoader() : classLoader;
-		this.packages = packages;
-		this.jars = jars;
+  /**
+   * Construct and search for interesting classes.
+   */
+  public BootupClassPathSearch(ClassLoader classLoader, List<String> packages, List<String> jars, String classPathReaderClassName) {
+    this.classLoader = (classLoader == null) ? getClass().getClassLoader() : classLoader;
+    this.packages = packages;
+    this.jars = jars;
     this.classPathReaderClassName = classPathReaderClassName;
-	}
 
-	public BootupClasses getBootupClasses() {
-		synchronized (monitor) {
-			
-			if (bootupClasses == null){
-				bootupClasses = search();
-			}
-			
-			return bootupClasses;
-		}
-	}
+    loadAndInitializeClassPathSearchServices();
+  }
 
-	/**
-	 * Search the classPath for the classes we are interested in.
-	 */
-	private BootupClasses search() {
-		synchronized (monitor) {
-			try {
-				
-				BootupClasses bc = new BootupClasses();
+  public BootupClasses getBootupClasses() {
+    synchronized (monitor) {
 
-				long st = System.currentTimeMillis();
+      if (bootupClasses == null) {
+        bootupClasses = search();
+      }
 
-				ClassPathSearchFilter filter = createFilter();
+      return bootupClasses;
+    }
+  }
 
-				ClassPathSearch finder = new ClassPathSearch(classLoader, filter, bc, classPathReaderClassName);
+  /**
+   * Search the classPath for the classes we are interested in.
+   */
+  private BootupClasses search() {
+    synchronized (monitor) {
+      try {
 
-				finder.findClasses();
-				Set<String> jars = finder.getJarHits();
-				Set<String> pkgs = finder.getPackageHits();
+        BootupClasses bc = new BootupClasses();
 
-				long searchTime = System.currentTimeMillis() - st;
+        long st = System.currentTimeMillis();
 
-				String msg = "Classpath search hits in jars" + jars + " pkgs" + pkgs + "  searchTime[" + searchTime+ "]";
-				logger.info(msg);
+        ClassPathSearchFilter filter = createFilter();
 
-				return bc;
+        Set<String> foundJars = new HashSet<String>();
+        Set<String> foundPkgs = new HashSet<String>();
 
-			} catch (Exception ex) {
-				String msg = "Error in classpath search (looking for entities etc)";
-				throw new RuntimeException(msg, ex);
-			}
-		}
-	}
-
-	private ClassPathSearchFilter createFilter() {
-
-		ClassPathSearchFilter filter = new ClassPathSearchFilter();
-		filter.addDefaultExcludePackages();
-
-        if (packages != null && packages.size() > 0) {
-            for (String packageName : packages) {
-                filter.includePackage(packageName);
-            }
-
-	          // if they specified include packages, they don't want by default to include everything
-	          filter.setDefaultPackageMatch(false);
+        for (ClassPathSearchService finder : this.classPathSearchServices) {
+          finder.init(classLoader, filter, bc, classPathReaderClassName);
+          finder.findClasses();
+          foundJars.addAll(finder.getJarHits());
+          foundPkgs.addAll(finder.getPackageHits());
         }
 
-        if (jars != null && jars.size() > 0) {
-            for (String jarName : jars) {
-                filter.includeJar(jarName);
-            }
+        long searchTime = System.currentTimeMillis() - st;
 
-	          // if they specified jars to specifically include, they don't want everything included
-	          filter.setDefaultJarMatch(false);
-        }
+        logger.info("Classpath search hits in jars {} pkgs {} searchTime [{}]", foundJars, foundPkgs, searchTime);
+        return bc;
 
-		return filter;
-	}
+      } catch (Exception ex) {
+        String msg = "Error in classpath search (looking for entities etc)";
+        throw new RuntimeException(msg, ex);
+      }
+    }
+  }
+
+  private ClassPathSearchFilter createFilter() {
+
+    ClassPathSearchFilter filter = new ClassPathSearchFilter();
+    filter.addDefaultExcludePackages();
+
+    if (packages != null && packages.size() > 0) {
+      for (String packageName : packages) {
+        filter.includePackage(packageName);
+      }
+
+      // if they specified include packages, they don't want by default to include everything
+      filter.setDefaultPackageMatch(false);
+    }
+
+    if (jars != null && jars.size() > 0) {
+      for (String jarName : jars) {
+        filter.includeJar(jarName);
+      }
+
+      // if they specified jars to specifically include, they don't want everything included
+      filter.setDefaultJarMatch(false);
+    }
+
+    return filter;
+  }
+
+  /**
+   * Load and initialize all ClassPathSearchServices
+   */
+  private void loadAndInitializeClassPathSearchServices() {
+    if (this.classPathSearchServices == null) {
+      this.classPathSearchServices = new ArrayList<ClassPathSearchService>();
+    }
+
+    for (ClassPathSearchService searchService : ServiceLoader.load(ClassPathSearchService.class, classLoader)) {
+      this.classPathSearchServices.add(searchService);
+    }
+  }
 }
