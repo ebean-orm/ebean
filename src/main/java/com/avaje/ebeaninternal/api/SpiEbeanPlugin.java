7f06863b195c379afa0556d127c04e85477ba6f4
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-06 12:03:32 +1200

    #374 - SpiEbeanPlugin API change : DatabasePlatform dbPlatform parameter removed

--- src/main/java/com/avaje/ebeaninternal/api/SpiEbeanPlugin.java
+++ src/main/java/com/avaje/ebeaninternal/api/SpiEbeanPlugin.java
@@ -1,7 +1,6 @@
 package com.avaje.ebeaninternal.api;
 
 import com.avaje.ebean.config.ServerConfig;
-import com.avaje.ebean.config.dbplatform.DatabasePlatform;
 
 /**
  * Allows us to have more than one plugin (the ddl  generator) active based on flags.
@@ -14,10 +13,9 @@ public interface SpiEbeanPlugin {
    * initializes the plugin.
    *
    * @param server - the ebean server with extensions for all of the Ebean internals
-   * @param dbPlatform - the database we are using - this is available from the server, but it is provided for convenience
    * @param serverConfig - the configured server information. It allows access to pre-collected information (but not the collector PropertySource, yet)
    */
-  void setup(SpiEbeanServer server, DatabasePlatform dbPlatform, ServerConfig serverConfig);
+  void setup(SpiEbeanServer server, ServerConfig serverConfig);
 
   /**
    * Execute the plugin (generate DDL for example).
