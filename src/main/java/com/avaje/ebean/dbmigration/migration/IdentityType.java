1603c691562f89ad6a98223e400ed5e488b0bfe0
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-07 12:45:45 +1200

    #374 - IdentityType lowered

--- src/main/java/com/avaje/ebean/dbmigration/migration/IdentityType.java
+++ src/main/java/com/avaje/ebean/dbmigration/migration/IdentityType.java
@@ -2,6 +2,7 @@
 package com.avaje.ebean.dbmigration.migration;
 
 import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -13,11 +14,11 @@ import javax.xml.bind.annotation.XmlType;
  * <pre>
  * &lt;simpleType name="identityType">
  *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
- *     &lt;enumeration value="IDENTITY"/>
- *     &lt;enumeration value="SEQUENCE"/>
- *     &lt;enumeration value="GENERATOR"/>
- *     &lt;enumeration value="EXTERNAL"/>
- *     &lt;enumeration value="DEFAULT"/>
+ *     &lt;enumeration value="identity"/>
+ *     &lt;enumeration value="sequence"/>
+ *     &lt;enumeration value="generator"/>
+ *     &lt;enumeration value="external"/>
+ *     &lt;enumeration value="default"/>
  *   &lt;/restriction>
  * &lt;/simpleType>
  * </pre>
@@ -27,18 +28,33 @@ import javax.xml.bind.annotation.XmlType;
 @XmlEnum
 public enum IdentityType {
 
-    IDENTITY,
-    SEQUENCE,
-    GENERATOR,
-    EXTERNAL,
-    DEFAULT;
+    @XmlEnumValue("identity")
+    IDENTITY("identity"),
+    @XmlEnumValue("sequence")
+    SEQUENCE("sequence"),
+    @XmlEnumValue("generator")
+    GENERATOR("generator"),
+    @XmlEnumValue("external")
+    EXTERNAL("external"),
+    @XmlEnumValue("default")
+    DEFAULT("default");
+    private final String value;
+
+    IdentityType(String v) {
+        value = v;
+    }
 
     public String value() {
-        return name();
+        return value;
     }
 
     public static IdentityType fromValue(String v) {
-        return valueOf(v);
+        for (IdentityType c: IdentityType.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
     }
 
 }
