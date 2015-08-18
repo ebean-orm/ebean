9ca1a87f8b9c7ca1a5496303e98c55f4bbb20b76
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-18 22:13:12 +1200

    No effective change - add missing javadoc

--- src/main/java/com/avaje/ebean/dbmigration/DbOffline.java
+++ src/main/java/com/avaje/ebean/dbmigration/DbOffline.java
@@ -5,7 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Helper to indicate that an EbeanServer should come up offline
+ * typically for DDL generation purposes.
  */
 public class DbOffline {
 
@@ -15,28 +16,43 @@ public class DbOffline {
 
   private static boolean runningMigration;
 
+  /**
+   * Set the platform to use when creating the next EbeanServer instance.
+   */
   public static void setPlatform(DbPlatformName dbPlatform) {
     System.setProperty(KEY, dbPlatform.name());
   }
 
+  /**
+   * Set the platform to use when creating the next EbeanServer instance.
+   */
   public static void setPlatform(String platformName) {
     System.setProperty(KEY, platformName);
   }
 
+  /**
+   * Return the platform to use when creating the next EbeanServer instance.
+   */
   public static String getPlatform() {
     return System.getProperty(KEY);
   }
 
+  /**
+   * Bring up the next EbeanServer instance using the H2 platform.
+   */
   public static void asH2() {
     setPlatform(DbPlatformName.H2);
   }
 
+  /**
+   * Return true if the offline platform has been set.
+   */
   public static boolean isSet() {
     return getPlatform() != null;
   }
 
   /**
-   * Return true if the migration is runing. This typically means don't run the
+   * Return true if the migration is running. This typically means don't run the
    * plugins like full DDL generation.
    */
   public static boolean isRunningMigration() {
