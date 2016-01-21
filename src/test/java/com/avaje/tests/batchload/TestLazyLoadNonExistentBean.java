0e46184656015a573a732037a21e6942a6f0cc13
Robin Bygrave <robin.bygrave@gmail.com>
2016-01-21 16:03:51 +1300

    No effective change - Rename test class

---
+++ src/test/java/com/avaje/tests/batchload/TestLazyLoadNonExistentBean.java
@@ -0,0 +1,31 @@
+package com.avaje.tests.batchload;
+
+import java.util.UUID;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.avaje.ebean.BaseTestCase;
+import com.avaje.ebean.Ebean;
+import com.avaje.tests.model.basic.UUOne;
+
+public class TestLazyLoadNonExistentBean extends BaseTestCase {
+
+  @Test
+  public void testSimple() {
+
+    UUID uuid = UUID.randomUUID();
+    UUOne one = Ebean.getReference(UUOne.class, uuid);
+
+    try {
+      // invoke lazy loading
+      one.getName();
+      Assert.assertTrue(false);
+    } catch (EntityNotFoundException e) {
+      // expecting this
+      Assert.assertTrue(true);
+    }
+  }
+}
