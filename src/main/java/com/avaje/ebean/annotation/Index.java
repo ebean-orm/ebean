f5c8d8896f96ced2260c5d3c5bfed39d484eb507
Roland Praml <praml@foconis.de>
2016-08-22 10:00:13 +0200

    Used @Repeatable feature from JDK8 for @Indices

--- src/main/java/com/avaje/ebean/annotation/Index.java
+++ src/main/java/com/avaje/ebean/annotation/Index.java
@@ -1,6 +1,7 @@
 package com.avaje.ebean.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Indices.class)
 public @interface Index {
 
   /**
