da15d0b2c61d8a5d32c4e70217dd1409e1ccd8d9
Robin Bygrave <robin.bygrave@gmail.com>
2016-08-23 20:00:07 +1200

    No effective change - format javadoc

--- src/main/java/com/avaje/ebeaninternal/server/deploy/parse/AnnotationBase.java
+++ src/main/java/com/avaje/ebeaninternal/server/deploy/parse/AnnotationBase.java
@@ -1,15 +1,15 @@
 package com.avaje.ebeaninternal.server.deploy.parse;
 
+import com.avaje.ebean.config.NamingConvention;
+import com.avaje.ebean.config.dbplatform.DatabasePlatform;
+import com.avaje.ebeaninternal.server.deploy.meta.DeployBeanProperty;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.avaje.ebean.config.NamingConvention;
-import com.avaje.ebean.config.dbplatform.DatabasePlatform;
-import com.avaje.ebeaninternal.server.deploy.meta.DeployBeanProperty;
 
 /**
  * Provides some base methods for processing deployment annotations.
@@ -69,31 +69,28 @@ public abstract class AnnotationBase {
   protected <T extends Annotation> T find(DeployBeanProperty prop, Class<T> annClass) {
     T a = get(prop, annClass);
     if (a == null) {
-      a = findAnnotation(prop.getOwningType(),annClass);
+      a = findAnnotation(prop.getOwningType(), annClass);
     }
     return a;
   }
 
-  
+
   // this code is taken from the spring framework to find annotations recursively
-  
+
   /**
-   * Determine if the supplied {@link Annotation} is defined in the core JDK
-   * {@code java.lang.annotation} package.
+   * Determine if the supplied {@link Annotation} is defined in the core JDK {@code java.lang.annotation} package.
    */
   public static boolean isInJavaLangAnnotationPackage(Annotation annotation) {
     return annotation.annotationType().getName().startsWith("java.lang.annotation");
   }
-  
+
   /**
-   * Find a single {@link Annotation} of {@code annotationType} on the
-   * supplied {@link AnnotatedElement}.
-   * <p>Meta-annotations will be searched if the annotation is not
-   * <em>directly present</em> on the supplied element.
-   * <p><strong>Warning</strong>: this method operates generically on
-   * annotated elements. In other words, this method does not execute
-   * specialized search algorithms for classes or methods. It only
-   * traverses through Annotations!</p>
+   * Find a single {@link Annotation} of {@code annotationType} on the supplied {@link AnnotatedElement}.
+   * <p>
+   * Meta-annotations will be searched if the annotation is not <em>directly present</em> on the supplied element.
+   * <p>
+   * <strong>Warning</strong>: this method operates generically on annotated elements. In other words, this method
+   * does not execute specialized search algorithms for classes or methods. It only traverses through Annotations!
    */
   public static <A extends Annotation> A findAnnotation(AnnotatedElement annotatedElement, Class<A> annotationType) {
     if (annotationType == null) {
@@ -109,12 +106,10 @@ public abstract class AnnotationBase {
   }
 
   /**
-   * Find a single {@link Annotation} of {@code annotationType} on the
-   * supplied {@link clazz}.
-   * <p>Meta-annotations will be searched if the annotation is not
-   * <em>directly present</em> on the supplied element.
-   * <p><strong>Note</strong>: this method searches for annotations at 
-   * class & superClass(es)!</p>
+   * Find a single {@link Annotation} of {@code annotationType} on the supplied class.
+   * <p>Meta-annotations will be searched if the annotation is not directly present on
+   * the supplied element.
+   * <p><strong>Note</strong>: this method searches for annotations at class & superClass(es)!
    */
   public static <A extends Annotation> A findAnnotation(Class<?> clazz, Class<A> annotationType) {
     if (annotationType == null) {
@@ -136,28 +131,28 @@ public abstract class AnnotationBase {
       return null;
     }
   }
+
   /**
-   * Perform the search algorithm for {@link #findAnnotation(AnnotatedElement, Class)}
-   * avoiding endless recursion by tracking which annotations have already
-   * been <em>visited</em>.
+   * Perform the search algorithm avoiding endless recursion by tracking which
+   * annotations have already been visited.
    */
   @SuppressWarnings("unchecked")
   private static <A extends Annotation> A findAnnotation(AnnotatedElement annotatedElement, Class<A> annotationType, Set<Annotation> visited) {
-  
-      Annotation[] anns = annotatedElement.getDeclaredAnnotations();
-      for (Annotation ann : anns) {
-        if (ann.annotationType() == annotationType) {
-          return (A) ann;
+
+    Annotation[] anns = annotatedElement.getDeclaredAnnotations();
+    for (Annotation ann : anns) {
+      if (ann.annotationType() == annotationType) {
+        return (A) ann;
+      }
+    }
+    for (Annotation ann : anns) {
+      if (!isInJavaLangAnnotationPackage(ann) && visited.add(ann)) {
+        A annotation = findAnnotation(ann.annotationType(), annotationType, visited);
+        if (annotation != null) {
+          return annotation;
         }
       }
-      for (Annotation ann : anns) {
-        if (!isInJavaLangAnnotationPackage(ann) && visited.add(ann)) {
-          A annotation = findAnnotation((AnnotatedElement) ann.annotationType(), annotationType, visited);
-          if (annotation != null) {
-            return annotation;
-          }
-        }
-      }
+    }
     return null;
   }
 }
