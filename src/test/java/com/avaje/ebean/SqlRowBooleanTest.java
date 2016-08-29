1796c8b9bc4c3c45d7645b9fd9381ae2aa67bd6e
Robin Bygrave <robin.bygrave@gmail.com>
2016-08-29 21:50:14 +1200

    #817 - SqlRow getBoolean() does not treat numeric 1 value as true (with MySql and other DB's without native boolean type)   - Fix test for PG

--- src/test/java/com/avaje/ebean/SqlRowBooleanTest.java
+++ src/test/java/com/avaje/ebean/SqlRowBooleanTest.java
@@ -9,7 +9,7 @@ public class SqlRowBooleanTest {
   @Test
   public void getBoolean() {
 
-    SqlQuery sqlQuery = Ebean.createSqlQuery("SELECT 1 IS NOT NULL AS `ISNT_NULL`");
+    SqlQuery sqlQuery = Ebean.createSqlQuery("SELECT 1 IS NOT NULL AS ISNT_NULL");
     SqlRow row = sqlQuery.findUnique();
 
     Boolean value = row.getBoolean("ISNT_NULL");
