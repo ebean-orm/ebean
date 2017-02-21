b3b8f6b154ce1c433b1133bf009283585aa357a2
Rob Bygrave <robin.bygrave@gmail.com>
2017-02-21 21:57:11 +1300

    #973 - Refactor - Tidy internals of DefaultBeanLoader

--- src/main/java/io/ebeaninternal/server/core/DefaultBeanLoader.java
+++ src/main/java/io/ebeaninternal/server/core/DefaultBeanLoader.java
@@ -288,7 +288,7 @@ public class DefaultBeanLoader {
     }
 
     if (embeddedOwnerIndex > -1) {
-      query.select("id," + ebi.getProperty(embeddedOwnerIndex));
+      query.select(ebi.getProperty(embeddedOwnerIndex));
     }
 
     // don't collect AutoTune usage profiling information
