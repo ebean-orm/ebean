ce6d6269cdde806f1b728300ba4914e5fb67f75d
AWoelfel <alexander.woelfel@outlook.com>
2017-06-08 15:07:32 +0200

    Step to reproduce reset of dirty flag on L2 cached entities after lazy load

---
+++ src/test/java/org/tests/model/basic/L2CachedLazyDirtFlagResetBean.java
@@ -0,0 +1,51 @@
+package org.tests.model.basic;
+
+import io.ebean.annotation.Cache;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Cached bean for testing caching implementation.
+ */
+@Cache
+@Entity
+@Table(name = "l2_cached_lazy_dirt_flag_reset_bean")
+public class L2CachedLazyDirtFlagResetBean {
+
+  public Long getId() {
+    return id;
+  }
+
+  @Id
+  Long id;
+
+  String name;
+
+  public List<L2CachedLazyDirtFlagResetBeanChild> getChildren() {
+    return children;
+  }
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "parent")
+  List<L2CachedLazyDirtFlagResetBeanChild> children;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Long> someRichObjectMethod() {
+    List<Long> result = new ArrayList<Long>();
+    if(this.children != null) {
+      for (L2CachedLazyDirtFlagResetBeanChild relation : this.children) {
+        result.add(relation.getId());
+      }
+    }
+    return result;
+  }
+
+
+}
