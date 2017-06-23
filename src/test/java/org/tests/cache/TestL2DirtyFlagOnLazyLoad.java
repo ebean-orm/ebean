f1f2a416d379270214e0415f029a4b4b54095f1f
rob bygrave <robin.bygrave@gmail.com>
2017-06-23 23:19:22 +1200

    No effective change - reformat only

--- src/test/java/org/tests/cache/TestL2DirtyFlagOnLazyLoad.java
+++ src/test/java/org/tests/cache/TestL2DirtyFlagOnLazyLoad.java
@@ -1,13 +1,15 @@
 package org.tests.cache;
 
-import org.tests.model.basic.L2CachedLazyDirtFlagResetBean;
 import io.ebean.BaseTestCase;
+import io.ebean.BeanState;
 import io.ebean.Ebean;
 import io.ebean.Transaction;
 import org.junit.Test;
+import org.tests.model.basic.L2CachedLazyDirtFlagResetBean;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class TestL2DirtyFlagOnLazyLoad extends BaseTestCase {
 
@@ -16,10 +18,10 @@ public class TestL2DirtyFlagOnLazyLoad extends BaseTestCase {
 
     //PREPARE
     Transaction tx0 = Ebean.beginTransaction();
-      L2CachedLazyDirtFlagResetBean bean = new L2CachedLazyDirtFlagResetBean();
-      bean.setName("findById");
-      Ebean.save(bean);
-      //bean.save();
+    L2CachedLazyDirtFlagResetBean bean = new L2CachedLazyDirtFlagResetBean();
+    bean.setName("findById");
+    Ebean.save(bean);
+    //bean.save();
     tx0.commit();
 
     // clearAll() caches via the ServerCacheManager ...
@@ -28,28 +30,32 @@ public class TestL2DirtyFlagOnLazyLoad extends BaseTestCase {
 
     //TEST
     Transaction tx1 = Ebean.beginTransaction();
-      // load (and dont touch any related entity)
-      L2CachedLazyDirtFlagResetBean bean1 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
+    // load (and dont touch any related entity)
+    L2CachedLazyDirtFlagResetBean bean1 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
     tx1.commit();
 
 
     Transaction tx2 = Ebean.beginTransaction();
-      // load (and modify)
+    // load (and modify)
 
-      L2CachedLazyDirtFlagResetBean bean2 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
-      assertNotNull(bean2);
-      assertEquals("findById", bean2.getName());
-      bean2.setName("something");
-      bean2.someRichObjectMethod();
+    L2CachedLazyDirtFlagResetBean bean2 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
+    assertNotNull(bean2);
+    assertEquals("findById", bean2.getName());
+    bean2.setName("something");
+    BeanState beanState = Ebean.getBeanState(bean2);
+    assertTrue(beanState.isDirty());
+    //bean2.getChildren().size();
+    bean2.someRichObjectMethod();
+    assertTrue(beanState.isDirty());
 
-      Ebean.update(bean2);
+    Ebean.update(bean2);
     tx2.commit();
 
     Transaction tx3 = Ebean.beginTransaction();
-      // validation
+    // validation
 
-      L2CachedLazyDirtFlagResetBean bean3 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
-      assertEquals("something", bean3.getName());
+    L2CachedLazyDirtFlagResetBean bean3 = Ebean.find(L2CachedLazyDirtFlagResetBean.class, bean.getId());
+    assertEquals("something", bean3.getName());
 
     tx3.rollback();
 
