b58912448780cedddb69b9c23c9f0539414a9dbc
Roland Praml <praml@foconis.de>
2017-09-04 21:16:22 +0200

    ADD: DbmIgrationGenerateTest

---
+++ src/test/java/misc/migration/v1_2/ERef.java
@@ -0,0 +1,19 @@
+package misc.migration.v1_2;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "migtest_e_ref")
+public class ERef {
+  
+  @Id
+  Integer id;
+  
+  @OneToMany
+  List<EBasic> basics;
+}
