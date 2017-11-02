c732b21f8326548379abc4cf52720cfc766e2e23
Roland Praml <praml@foconis.de>
2017-11-02 17:07:16 +0100

    fix multi value bind for postgres

--- src/main/java/io/ebeaninternal/server/query/CQueryEngine.java
+++ src/main/java/io/ebeaninternal/server/query/CQueryEngine.java
@@ -127,7 +127,7 @@ public class CQueryEngine {
     if (t.isLogSummary()) {
       // log the error to the transaction log
       String errMsg = StringHelper.replaceStringMulti(e.getMessage(), new String[]{"\r", "\n"}, "\\n ");
-      String msg = "ERROR executing query, bindLog[" + bindLog + "] error[" + errMsg + "]";
+      String msg = "ERROR executing query, sql[" + sql + "] bindLog[" + bindLog + "] error[" + errMsg + "]";
       t.logSummary(msg);
     }
 
