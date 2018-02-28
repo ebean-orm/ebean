d36eb1b60e0f1e9ccc45edafab8fa5574ddefaec
Roland Praml <praml@foconis.de>
2018-02-28 21:28:30 +0100

    NEW: CustomDeployParser

--- src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanDescriptor.java
+++ src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanDescriptor.java
@@ -14,6 +14,7 @@ import io.ebean.event.BeanPostConstructListener;
 import io.ebean.event.BeanPostLoad;
 import io.ebean.event.BeanQueryAdapter;
 import io.ebean.event.changelog.ChangeLogFilter;
+import io.ebean.plugin.DeployBeanDescriptorMeta;
 import io.ebean.text.PathProperties;
 import io.ebean.util.CamelCaseHelper;
 import io.ebeaninternal.api.ConcurrencyMode;
@@ -46,7 +47,7 @@ import java.util.Map;
 /**
  * Describes Beans including their deployment information.
  */
-public class DeployBeanDescriptor<T> {
+public class DeployBeanDescriptor<T> implements DeployBeanDescriptorMeta {
 
   private static final Map<String, String> EMPTY_NAMED_QUERY = new HashMap<>();
 
@@ -233,7 +234,7 @@ public class DeployBeanDescriptor<T> {
   /**
    * Return the DeployBeanInfo for the given bean class.
    */
-  DeployBeanInfo<?> getDeploy(Class<?> cls) {
+  public DeployBeanInfo<?> getDeploy(Class<?> cls) {
     return manager.getDeploy(cls);
   }
 
@@ -611,6 +612,7 @@ public class DeployBeanDescriptor<T> {
    * Return the base table. Only properties mapped to the base table are by
    * default persisted.
    */
+  @Override
   public String getBaseTable() {
     return baseTable;
   }
@@ -719,6 +721,7 @@ public class DeployBeanDescriptor<T> {
   /**
    * Get a BeanProperty by its name.
    */
+  @Override
   public DeployBeanProperty getBeanProperty(String propName) {
     return propMap.get(propName);
   }
@@ -879,6 +882,7 @@ public class DeployBeanDescriptor<T> {
   /**
    * Return a collection of all BeanProperty deployment information.
    */
+  @Override
   public Collection<DeployBeanProperty> propertiesAll() {
     return propMap.values();
   }
@@ -956,6 +960,7 @@ public class DeployBeanDescriptor<T> {
    * itself doesn't change or the xml deployment order does not change.
    * </p>
    */
+  @Override
   public List<DeployBeanProperty> propertiesId() {
 
     if (idProperties == null) {
@@ -1151,5 +1156,15 @@ public class DeployBeanDescriptor<T> {
       namedRawSql = new HashMap<>();
     }
     namedRawSql.put(name, rawSql);
+  }
+  
+  @Override
+  public String getDiscriminatorColumn() {
+    return inheritInfo == null ? null : inheritInfo.getDiscriminatorColumn();
+  }
+  
+  @Override
+  public DeployBeanDescriptorMeta getDeployBeanDescriptorMeta(Class<?> propertyType) {
+    return getDeploy(propertyType).getDescriptor();
   }
 }
