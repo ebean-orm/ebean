d98c99a378c4ca89ac3979376bf8cdcf17420eaf
Roland Praml <praml@foconis.de>
2018-02-28 22:24:23 +0100

    FIX: Splitcolumns must return an empty array if string was empty

--- src/main/java/io/ebeaninternal/dbmigration/ddlgeneration/platform/SplitColumns.java
+++ src/main/java/io/ebeaninternal/dbmigration/ddlgeneration/platform/SplitColumns.java
@@ -6,6 +6,9 @@ public class SplitColumns {
    * Return as an array of string column names.
    */
   public static String[] split(String columns) {
+    if (columns == null || columns.isEmpty()) {
+      return new String[0];
+    }
     return columns.split(",");
   }
 
