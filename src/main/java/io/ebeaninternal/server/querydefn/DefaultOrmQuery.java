5687a53accc32510fe78e12435580dca9073f393
Roland Praml <praml@foconis.de>
2018-02-28 21:52:15 +0100

    NEW: fetchProperties

--- src/main/java/io/ebeaninternal/server/querydefn/DefaultOrmQuery.java
+++ src/main/java/io/ebeaninternal/server/querydefn/DefaultOrmQuery.java
@@ -1830,4 +1830,10 @@ public class DefaultOrmQuery<T> implements SpiQuery<T> {
   public boolean isOrderById() {
     return orderById;
   }
+
+  @Override
+  public Query<T> fetchProperties(String... elPaths) {
+    detail.fetchProperties(beanDescriptor, elPaths);
+    return this;
+  }
 }
