b2257e099802be364b6077a969fc060dc9206408
Roland Praml <praml@foconis.de>
2018-02-28 22:03:16 +0100

    Add test suite from rpraml

---
+++ src/test/java/misc/migration/v1_1/CKeyAssoc.java
@@ -0,0 +1,32 @@
+package misc.migration.v1_1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "migtest_ckey_assoc")
+public class CKeyAssoc {
+
+  @Id
+  Integer id;
+
+  String assocOne;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getAssocOne() {
+    return assocOne;
+  }
+
+  public void setAssocOne(String assocOne) {
+    this.assocOne = assocOne;
+  }
+
+}
