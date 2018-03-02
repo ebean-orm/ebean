1d95750b004400768bedd1dbabcd0580eb748127
Roland Praml <praml@foconis.de>
2018-03-02 02:04:44 +0100

    Revert "FIX: ReadOnlyDatasource must also be configured"
    This reverts commit 0e8b141461cd9072dee55450c7567c2c3d697fbe.

--- src/main/java/io/ebean/config/ServerConfig.java
+++ src/main/java/io/ebean/config/ServerConfig.java
@@ -2725,7 +2725,6 @@ public class ServerConfig {
    */
   protected void loadDataSourceSettings(PropertiesWrapper p) {
     dataSourceConfig.loadSettings(p.properties, name);
-    readOnlyDataSourceConfig.loadSettings(p.properties, name);
   }
 
   /**
