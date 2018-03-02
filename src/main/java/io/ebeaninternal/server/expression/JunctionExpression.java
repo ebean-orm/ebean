98e014f8c86366bd51da27b34d686a4cd3721b10
Rob Bygrave <robin.bygrave@gmail.com>
2018-03-02 14:30:38 +1300

    #1300 - NEW: FetchCountDistinct ... add example to javadoc + support chaining use via ExpressionList

--- src/main/java/io/ebeaninternal/server/expression/JunctionExpression.java
+++ src/main/java/io/ebeaninternal/server/expression/JunctionExpression.java
@@ -1,6 +1,7 @@
 package io.ebeaninternal.server.expression;
 
 import io.ebean.CacheMode;
+import io.ebean.CountDistinctOrder;
 import io.ebean.Expression;
 import io.ebean.ExpressionList;
 import io.ebean.FetchPath;
@@ -790,6 +791,11 @@ class JunctionExpression<T> implements SpiJunction<T>, SpiExpression, Expression
   @Override
   public Query<T> setDisableReadAuditing() {
     return exprList.setDisableReadAuditing();
+  }
+
+  @Override
+  public Query<T> setCountDistinct(CountDistinctOrder orderBy) {
+    return exprList.setCountDistinct(orderBy);
   }
 
   @Override
