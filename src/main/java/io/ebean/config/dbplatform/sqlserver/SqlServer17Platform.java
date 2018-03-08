5faec61cd2b90667226774fa86495d305f0d318f
Rob Bygrave <robin.bygrave@gmail.com>
2018-03-08 19:34:36 +1300

    #1318 - ENH: native UUID-support for SqlServer
    Merge branch 'native-uuid-sqlserver' of https://github.com/FOCONIS/ebean into FOCONIS-native-uuid-sqlserver
    
    # Conflicts:
    #	src/main/java/io/ebean/config/dbplatform/sqlserver/SqlServerBasePlatform.java

--- src/main/java/io/ebean/config/dbplatform/sqlserver/SqlServer17Platform.java
+++ src/main/java/io/ebean/config/dbplatform/sqlserver/SqlServer17Platform.java
@@ -1,6 +1,8 @@
 package io.ebean.config.dbplatform.sqlserver;
 
 import io.ebean.annotation.Platform;
+import io.ebean.config.dbplatform.DbPlatformType;
+import io.ebean.config.dbplatform.DbType;
 
 /**
  * Microsoft SQL Server platform - NVarchar UTF types and Sequence preference.
@@ -11,6 +13,8 @@ public class SqlServer17Platform extends SqlServerBasePlatform {
     super();
     this.platform = Platform.SQLSERVER17;
     this.columnAliasPrefix = null;
+    this.nativeUuidType = true;
+    this.dbTypeMap.put(DbType.UUID, new DbPlatformType("uniqueidentifier", false));
   }
 
 }
