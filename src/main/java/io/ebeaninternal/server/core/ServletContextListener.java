fa4af4ef98ffbea716a3ce906065f7f36a8b80d9
rob bygrave <robin.bygrave@gmail.com>
2018-05-27 15:41:51 +1200

    Revert "#1374 - Remove io.ebeaninternal.server.core.ServletContextListener - should be unused"
    This reverts commit 95737cd3c56f9adab311510fab0b4e3bac020a7d.

---
+++ src/main/java/io/ebeaninternal/server/core/ServletContextListener.java
@@ -0,0 +1,32 @@
+package io.ebeaninternal.server.core;
+
+import io.ebeaninternal.server.lib.ShutdownManager;
+
+import javax.servlet.ServletContextEvent;
+
+/**
+ * Listens for webserver server starting and stopping events.
+ * <p>
+ * Register this listener in the web.xml configuration file. This will listen
+ * for startup and shutdown events.
+ * </p>
+ */
+public class ServletContextListener implements javax.servlet.ServletContextListener {
+
+  /**
+   * The servlet container is stopping.
+   */
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    ShutdownManager.shutdown();
+  }
+
+  /**
+   * Do nothing on startup.
+   */
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+
+  }
+
+}
