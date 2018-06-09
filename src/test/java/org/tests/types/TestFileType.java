8fb197d1abf88135c9c4d3b2277602b9f7764162
Roland Praml <praml@foconis.de>
2018-06-10 00:53:19 +0200

    ADD: Testcase to detect if streams are closed properly

--- src/test/java/org/tests/types/TestFileType.java
+++ src/test/java/org/tests/types/TestFileType.java
@@ -2,13 +2,23 @@ package org.tests.types;
 
 import io.ebean.BaseTestCase;
 import io.ebean.Ebean;
+import io.ebean.Transaction;
+
 import org.tests.model.types.SomeFileBean;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.URL;
 
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class TestFileType extends BaseTestCase {
 
@@ -96,6 +106,84 @@ public class TestFileType extends BaseTestCase {
     Ebean.save(bean3);
 
     Ebean.delete(bean3);
+  }
+
+  @Test
+  public void test_canDeleteIfStreamIsOpen() throws IOException {
+    File tempFile = File.createTempFile("testfile", "txt");
+    try (PrintStream ps = new PrintStream(tempFile)) {
+      ps.println("Hello World!");
+    }
+    InputStream is = new BufferedInputStream(new FileInputStream(tempFile));
+    byte[] buf = new byte[4096];
+    is.read(buf);
+    // if stream held open, delete will fail
+    assertFalse(tempFile.delete());
+    is.close();
+    assertTrue(tempFile.delete());
+  }
+
+  @Test
+  public void test_closeFileStreamUnbatched() throws IOException {
+    File tempFile = File.createTempFile("testfile", "txt");
+    try (PrintStream ps = new PrintStream(tempFile)) {
+      ps.println("Hello World!");
+    }
+
+    SomeFileBean bean0 = new SomeFileBean();
+    bean0.setName("tempBeanUnbatched");
+    bean0.setContent(tempFile);
+    Ebean.save(bean0);
+
+    assertTrue(tempFile.delete());
+  }
+
+  @Test
+  public void test_closeFileStreamBatched() throws IOException {
+    File tempFile = File.createTempFile("testfile", "txt");
+    try (PrintStream ps = new PrintStream(tempFile)) {
+      ps.println("Hello World!");
+    }
+    try (Transaction txn = Ebean.beginTransaction()) {
+      txn.setBatchSize(30);
+      txn.setBatchMode(true);
+
+      SomeFileBean bean0 = new SomeFileBean();
+      bean0.setName("tempBeanBatched");
+      bean0.setContent(tempFile);
+      Ebean.save(bean0);
+
+      txn.commit();
+    }
+
+    assertTrue(tempFile.delete());
+
+  }
+
+  @Test
+  public void test_queryFileStream() throws IOException {
+    File tempFile0 = File.createTempFile("testfile", "txt");
+    try (PrintStream ps = new PrintStream(tempFile0)) {
+      ps.println("Search String");
+    }
+
+    SomeFileBean bean0 = new SomeFileBean();
+    bean0.setName("bean to find");
+    bean0.setContent(tempFile0);
+    Ebean.save(bean0);
+
+    File tempFile1 = File.createTempFile("testfile", "txt");
+    try (PrintStream ps = new PrintStream(tempFile1)) {
+      ps.println("Search String");
+    }
+
+    // find all beans that have the same file content
+    SomeFileBean bean1 = Ebean.find(SomeFileBean.class).where().eq("content",tempFile1).findOne();
+    assertNotNull(bean1);
+    assertThat(bean1.getName()).isEqualTo("bean to find");
+
+    assertTrue(tempFile1.delete()); // check if file is hold by the query
+    assertTrue(tempFile0.delete()); // check if file is hold by the inssert
   }
 
   private File getFile(String resource) {
