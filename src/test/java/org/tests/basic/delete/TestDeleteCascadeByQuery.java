27edf934fb80fc6f2cd2bbfcc1b1257716a41b60
Michael Benz <michael.benz@foconis.de>
2018-07-04 19:36:05 +0200

    moved init and cleanup code to @Before and @After JUnit methods and added another test that shows Ebean.delete() works as expected

--- src/test/java/org/tests/basic/delete/TestDeleteCascadeByQuery.java
+++ src/test/java/org/tests/basic/delete/TestDeleteCascadeByQuery.java
@@ -6,6 +6,8 @@ import io.ebean.BaseTestCase;
 import io.ebean.Ebean;
 import io.ebean.Query;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.tests.model.onetoone.OtoUser;
@@ -13,8 +15,40 @@ import org.tests.model.onetoone.OtoUserOptional;
 
 public class TestDeleteCascadeByQuery extends BaseTestCase {
   
-  Query<OtoUserOptional> userOptionalQuery = Ebean.find(OtoUserOptional.class);
-  Query<OtoUser> userQuery = Ebean.find(OtoUser.class);
+  private OtoUser testUser;
+  private OtoUserOptional userOptional;
+  private Query<OtoUserOptional> userOptionalQuery = Ebean.find(OtoUserOptional.class);
+  private Query<OtoUser> userQuery = Ebean.find(OtoUser.class);
+  
+  /**
+   * Init each test. Delete all existing beans. Then create OtoUser, add OtoUserOptional and save.
+   */
+  @Before
+  public void init() {
+    Ebean.deleteAll(userQuery.findList());
+    Ebean.deleteAll(userOptionalQuery.findList());
+    
+    userOptional = new OtoUserOptional();
+    Ebean.save(userOptional);
+    testUser = new OtoUser();
+    testUser.setOptional(userOptional);
+    Ebean.save(testUser);
+  }
+  
+  /**
+   * Test that validates deleting a bean using Ebean.delete() respects the CascadeType.DELETE 
+   * setting.
+   */
+  @Test
+  public void testDeleteCascadeByEbeanDelete() {
+
+    assertThat(Ebean.delete(testUser)).isTrue();
+
+    assertThat(userOptionalQuery.findCount())
+      .overridingErrorMessage("Entity OtoUserOptional found. Ebean.delete() on the user "
+          + "did not delete the OneToOne mapped entity as set with CascadeType.ALL")
+      .isEqualTo(0);
+  }
   
   /**
    * Test that validates deleting a bean with OneToOne mapping with a query respects the 
@@ -22,39 +56,21 @@ public class TestDeleteCascadeByQuery extends BaseTestCase {
    */
   @Test
   public void testDeleteCascadeByQuery() {
-
-    // Check that both models do not exist
-    assertNoUserOrOptionalExist();
-
-    // Create OtoUser and assign OtoUserOptional to it then save
-    createUserAndOptional();
     
-    // Deleting the testUser using Ebean.delete() works
-    // Ebean.delete(testUser);
-    
-    // Deleting the testUser using the Ebean query
     assertThat(userQuery.delete()).isEqualTo(1);
-    
-    // Check that both models are deleted (CascadeType.DELETE was respected)
-    assertNoUserOrOptionalExist();
 
-  }
-  
-  private OtoUser createUserAndOptional() {
-    OtoUserOptional userOptional = new OtoUserOptional();
-    OtoUser testUser = new OtoUser();
-    testUser.setOptional(userOptional);
-    Ebean.save(userOptional);
-    Ebean.save(testUser);
-    return testUser;
-  }
-  
-  private void assertNoUserOrOptionalExist() {
     assertThat(userOptionalQuery.findCount())
-      .overridingErrorMessage("Entity OtoUserOptional found.")
+      .overridingErrorMessage("Entity OtoUserOptional found. Ebean query delete() on the user "
+          + "did not delete the OneToOne mapped entity as set with CascadeType.ALL")
       .isEqualTo(0);
-    assertThat(userQuery.findCount())
-      .overridingErrorMessage("Entity OtoUser found.")
-      .isEqualTo(0);
+  }
+  
+  /**
+   * Cleanup - Delete all existing beans for the next test.
+   */
+  @After
+  public void cleanup() {
+    Ebean.deleteAll(userQuery.findList());
+    Ebean.deleteAll(userOptionalQuery.findList());
   }
 }
