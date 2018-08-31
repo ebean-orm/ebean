7809858634c3406cfc69ffc699af27a2af7b1323
rob bygrave <robin.bygrave@gmail.com>
2018-08-31 21:54:48 +1200

    #1478 - MySql DDL, invalid comment included resulting in PersistenceException: Failed to run script

--- src/test/resources/dbmigration/migrationtest/mysql/I__create_procs.sql
+++ src/test/resources/dbmigration/migrationtest/mysql/I__create_procs.sql
@@ -2,21 +2,21 @@
 DROP PROCEDURE IF EXISTS usp_ebean_drop_foreign_keys;
 
 delimiter $$
-------------------------------------------------------------------------------
+--
 -- PROCEDURE: usp_ebean_drop_foreign_keys TABLE, COLUMN
 -- deletes all constraints and foreign keys referring to TABLE.COLUMN
-------------------------------------------------------------------------------
+--
 CREATE PROCEDURE usp_ebean_drop_foreign_keys(IN p_table_name VARCHAR(255), IN p_column_name VARCHAR(255))
 BEGIN
   DECLARE done INT DEFAULT FALSE;
   DECLARE c_fk_name CHAR(255);
-  DECLARE curs CURSOR FOR SELECT CONSTRAINT_NAME from information_schema.KEY_COLUMN_USAGE 
+  DECLARE curs CURSOR FOR SELECT CONSTRAINT_NAME from information_schema.KEY_COLUMN_USAGE
     WHERE TABLE_SCHEMA = DATABASE() and TABLE_NAME = p_table_name and COLUMN_NAME = p_column_name
       AND REFERENCED_TABLE_NAME IS NOT NULL;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN curs;
-  
+
   read_loop: LOOP
     FETCH curs INTO c_fk_name;
     IF done THEN
@@ -26,7 +26,7 @@ BEGIN
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
   END LOOP;
-  
+
   CLOSE curs;
 END
 $$
@@ -34,10 +34,10 @@ $$
 DROP PROCEDURE IF EXISTS usp_ebean_drop_column;
 
 delimiter $$
--------------------------------------------------------------------------------------
+--
 -- PROCEDURE: usp_ebean_drop_column TABLE, COLUMN
 -- deletes the column and ensures that all indices and constraints are dropped first
--------------------------------------------------------------------------------------
+--
 CREATE PROCEDURE usp_ebean_drop_column(IN p_table_name VARCHAR(255), IN p_column_name VARCHAR(255))
 BEGIN
   CALL usp_ebean_drop_foreign_keys(p_table_name, p_column_name);
