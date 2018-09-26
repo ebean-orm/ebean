9734ae57875ce688d8c9bab5d4c0558ac5507e4a
Rob Bygrave <robin.bygrave@gmail.com>
2018-09-26 21:33:27 +1200

    #1490 - FIX: withQuote_when_match for different platforms

--- src/test/java/io/ebeaninternal/server/deploy/DeployPropertyParserTest.java
+++ src/test/java/io/ebeaninternal/server/deploy/DeployPropertyParserTest.java
@@ -3,7 +3,6 @@ package io.ebeaninternal.server.deploy;
 import io.ebean.BaseTestCase;
 import io.ebean.annotation.ForPlatform;
 import io.ebean.annotation.Platform;
-
 import org.junit.Test;
 import org.tests.model.basic.Address;
 import org.tests.model.basic.BWithQIdent;
@@ -56,24 +55,28 @@ public class DeployPropertyParserTest extends BaseTestCase {
   }
 
   @Test
+  public void withExplicitQuote_all_platforms() {
+    assertThat(withQuoteParser().parse("t0.`CODE` like ?")).isEqualTo("t0.`CODE` like ?");
+    assertThat(withQuoteParser().parse("t0.[CODE] like ?")).isEqualTo("t0.[CODE] like ?");
+    assertThat(withQuoteParser().parse("t0.\"CODE\" like ?")).isEqualTo("t0.\"CODE\" like ?");
+  }
+
+  @Test
   @ForPlatform(value = {Platform.H2, Platform.POSTGRES})
   public void withQuote_when_match_h2() {
     assertThat(withQuoteParser().parse("name like ?")).isEqualTo("${}\"Name\" like ?");
-    assertThat(withQuoteParser().parse("t0.\"CODE\" like ?")).isEqualTo("t0.\"CODE\" like ?");
   }
 
   @Test
   @ForPlatform(value = Platform.SQLSERVER)
   public void withQuote_when_match_sqlserver() {
     assertThat(withQuoteParser().parse("name like ?")).isEqualTo("${}[Name] like ?");
-    assertThat(withQuoteParser().parse("t0.[CODE] like ?")).isEqualTo("t0.[CODE] like ?");
   }
 
   @Test
   @ForPlatform(value = Platform.MYSQL)
   public void withQuote_when_match_mysql() {
     assertThat(withQuoteParser().parse("name like ?")).isEqualTo("${}`Name` like ?");
-    assertThat(withQuoteParser().parse("t0.`CODE` like ?")).isEqualTo("t0.`CODE` like ?");
   }
 
   @Test
