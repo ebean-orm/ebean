0ce0ab34e9b05b46736be211fac4499cb947e638
Roland Praml <praml@foconis.de>
2019-02-02 17:15:30 +0100

    fix tablename  (reserved word in mariadb)

--- src/test/java/org/tests/query/cache/Position.java
+++ src/test/java/org/tests/query/cache/Position.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "e_position")
 public class Position {
 	@Id
 	@GeneratedValue
