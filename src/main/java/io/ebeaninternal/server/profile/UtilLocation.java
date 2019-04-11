d32559baf46129a0627e7081796c98d1fab3953b
rob bygrave <robin.bygrave@gmail.com>
2019-04-12 02:25:05 +1200

    #1674 - For ProfileLocation use in constructors trim label <init> to init .... (for safe label)

--- src/main/java/io/ebeaninternal/server/profile/UtilLocation.java
+++ src/main/java/io/ebeaninternal/server/profile/UtilLocation.java
@@ -7,8 +7,19 @@ final class UtilLocation {
     if (pos == -1) {
       return shortDescription;
     } else {
-      return shortDescription.substring(0, pos);
+      return trimInit(shortDescription.substring(0, pos));
     }
+  }
+
+  /**
+   * Trim constructor init to be "safe" without greater than or less than chars.
+   */
+  private static String trimInit(String desc) {
+    if (desc.endsWith("<init>")) {
+      // trim to make a safe label
+      return desc.substring(0, desc.length() - 6) + "init";
+    }
+    return desc;
   }
 
 }
--- src/test/java/org/tests/profile/ProfileLocationTest.java
+++ src/test/java/org/tests/profile/ProfileLocationTest.java
@@ -9,6 +9,8 @@ public class ProfileLocationTest {
 
   private static ProfileLocation loc = ProfileLocation.create(12, "foo");
 
+  private static ProfileLocation loc2 = ProfileLocation.create();
+
   private String doIt() {
     return loc.obtain();
   }
@@ -16,13 +18,30 @@ public class ProfileLocationTest {
   @Test
   public void test_obtain() {
 
-    assertThat(doIt()).isEqualTo("org.tests.profile.ProfileLocationTest.doIt(ProfileLocationTest.java:13)");
-    assertThat(loc.shortDescription()).isEqualTo("ProfileLocationTest.doIt(ProfileLocationTest.java:13)");
+    assertThat(doIt()).isEqualTo("org.tests.profile.ProfileLocationTest.doIt(ProfileLocationTest.java:15)");
+    assertThat(loc.shortDescription()).isEqualTo("ProfileLocationTest.doIt(ProfileLocationTest.java:15)");
     assertThat(loc.label()).isEqualTo("ProfileLocationTest.doIt");
   }
 
   @Test
   public void test_add() {
     loc.add(100);
+  }
+
+  @Test
+  public void test_constructor() {
+
+    Other other = new Other();
+    other.hashCode();
+
+    assertThat(loc2.label()).isEqualTo("ProfileLocationTest$Other.init");
+    assertThat(loc2.shortDescription()).isEqualTo("ProfileLocationTest$Other.<init>(ProfileLocationTest.java:44)");
+  }
+
+  static class Other {
+
+    Other() {
+      loc2.obtain();
+    }
   }
 }
