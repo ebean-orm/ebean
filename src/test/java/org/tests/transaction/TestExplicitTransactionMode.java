cf31830cbc263a2612bda62903db198a11d6af59
rob bygrave <robin.bygrave@gmail.com>
2019-04-26 13:21:19 +1200

    #1677 - No effective change - Add test but not failing etc

--- src/test/java/org/tests/transaction/TestExplicitTransactionMode.java
+++ src/test/java/org/tests/transaction/TestExplicitTransactionMode.java
@@ -1,5 +1,7 @@
 package org.tests.transaction;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import io.ebean.BaseTestCase;
 import io.ebean.EbeanServer;
 import io.ebean.EbeanServerFactory;
@@ -12,15 +14,19 @@ import io.ebean.config.properties.PropertiesLoader;
 import io.ebean.datasource.DataSourceConfig;
 import io.ebean.datasource.DataSourcePool;
 import io.ebean.datasource.pool.ConnectionPool;
+import io.ebeaninternal.server.type.ScalarTypeLocalDate;
 import org.junit.Test;
 import org.tests.model.basic.UTDetail;
 import org.tests.model.basic.UTMaster;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Properties;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -55,8 +61,11 @@ public class TestExplicitTransactionMode extends BaseTestCase {
     config.setDdlGenerate(true);
     config.setDdlRun(true);
     config.setDdlExtra(false);
+    config.addClass(ScalarTypeLocalDateAsString.class);
 
     EbeanServer ebeanServer = EbeanServerFactory.create(config);
+
+    testJsonScalarType(ebeanServer);
 
     Query<UTMaster> query = ebeanServer.find(UTMaster.class);
     List<UTMaster> details = query.findList();
@@ -103,6 +112,29 @@ public class TestExplicitTransactionMode extends BaseTestCase {
       Query<UTMaster> query3 = ebeanServer.find(UTMaster.class);
       details = ebeanServer.extended().findList(query3, otherTxn);
       assertEquals(3, details.size());
+    }
+  }
+
+  private void testJsonScalarType(EbeanServer ebeanServer) {
+    UTMaster bean = new UTMaster("one1");
+    bean.setDate(LocalDate.of(2019, 04, 20));
+
+    String json = ebeanServer.json().toJson(bean);
+    assertThat(json).isEqualTo("{\"name\":\"one1\",\"date\":\"2019-04-20\"}");
+    UTMaster jsonMaster = ebeanServer.json().toBean(UTMaster.class, json);
+    assertThat(jsonMaster.getDate()).isEqualTo(LocalDate.of(2019, 4, 20));
+  }
+
+  public static class ScalarTypeLocalDateAsString extends ScalarTypeLocalDate {
+
+    @Override
+    public LocalDate jsonRead(JsonParser parser) throws IOException {
+      return super.jsonRead(parser);
+    }
+
+    @Override
+    public void jsonWrite(JsonGenerator writer, LocalDate value) throws IOException {
+      writer.writeString(value.toString());
     }
   }
 }
