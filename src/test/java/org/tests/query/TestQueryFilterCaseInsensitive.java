b4782425f8c5c7b0c2941f94107ee353fc81737d
rob bygrave <robin.bygrave@gmail.com>
2019-05-08 23:38:25 +1200

    #1695 - For MySql like remove the binary keyword

--- src/test/java/org/tests/query/TestQueryFilterCaseInsensitive.java
+++ src/test/java/org/tests/query/TestQueryFilterCaseInsensitive.java
@@ -1,7 +1,9 @@
 package org.tests.query;
 
 import io.ebean.BaseTestCase;
-import io.ebean.Ebean;
+import io.ebean.DB;
+import io.ebean.annotation.IgnorePlatform;
+import io.ebean.annotation.Platform;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tests.model.basic.Customer;
@@ -18,33 +20,35 @@ public class TestQueryFilterCaseInsensitive extends BaseTestCase {
     ResetBasicData.reset();
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testEq() {
 
     // Note: this test uses only customer#1..#4
 
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .eq("name", "ROB") // case match
         .le("id", 4).findList();
 
     assertThat(customers).isEmpty();
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .ieq("name", "ROB") // case insensitive match
         .le("id", 4).findList();
 
     assertThat(customers).hasSize(1);
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testNe() {
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .ne("name", "ROB") // case match
         .le("id", 4).findList();
 
     assertThat(customers).hasSize(4);
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .ine("name", "ROB") // case insensitive match
         .le("id", 4).findList();
 
@@ -52,30 +56,32 @@ public class TestQueryFilterCaseInsensitive extends BaseTestCase {
 
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testLike() {
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .like("name", "%O%") // case match
         .le("id", 4).findList();
 
     assertThat(customers).isEmpty();
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .ilike("name", "%O%") // case insensitive match
         .le("id", 4).findList();
 
     assertThat(customers).hasSize(4); // Rob / Fiona / Cust No address / NocCust
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testContains() {
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .contains("name", "O") // case match
         .le("id", 4).findList();
 
     assertThat(customers).isEmpty();
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .icontains("name", "O") // case insensitive match
         .le("id", 4).findList();
 
@@ -83,30 +89,32 @@ public class TestQueryFilterCaseInsensitive extends BaseTestCase {
 
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testStartsWith() {
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .startsWith("name", "RO") // case match
         .le("id", 4).findList();
 
     assertThat(customers).isEmpty();
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .istartsWith("name", "RO") // case insensitive match
         .le("id", 4).findList();
 
     assertThat(customers).hasSize(1);
   }
 
+  @IgnorePlatform(Platform.MYSQL)
   @Test
   public void testEndsWith() {
-    List<Customer> customers = Ebean.find(Customer.class).where()
+    List<Customer> customers = DB.find(Customer.class).where()
         .endsWith("name", "OB") // case match
         .le("id", 4).findList();
 
     assertThat(customers).isEmpty();
 
-    customers = Ebean.find(Customer.class).where()
+    customers = DB.find(Customer.class).where()
         .iendsWith("name", "OB") // case insensitive match
         .le("id", 4).findList();
 
