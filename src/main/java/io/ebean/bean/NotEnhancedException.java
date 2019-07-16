945175c533b72c76cc47c3305c9a5b3d65143f51
Roland Praml <praml@foconis.de>
2019-07-16 13:07:40 +0200

    Throwing NotEnhancedException on _ebean_* method instead of enforce to implement them (#1755)

---
+++ src/main/java/io/ebean/bean/NotEnhancedException.java
@@ -0,0 +1,12 @@
+package io.ebean.bean;
+
+/**
+ * This exception is thrown, when the default implementation of EntityBean is not or not properly overwritten by enhancer.
+ *
+ * @author Roland Praml, FOCONIS AG
+ *
+ */
+public class NotEnhancedException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+}
