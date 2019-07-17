0663a97941b4b915aac4eac49eb10f08ffce076d
rob bygrave <robin.bygrave@gmail.com>
2019-07-17 13:48:12 +1200

    No effective change - update test for TestTransientMap

--- src/test/java/org/tests/basic/type/TestTransientMap.java
+++ src/test/java/org/tests/basic/type/TestTransientMap.java
@@ -1,18 +1,32 @@
 package org.tests.basic.type;
 
 import io.ebean.BaseTestCase;
-import io.ebean.Ebean;
+import io.ebean.DB;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestTransientMap extends BaseTestCase {
 
   @Test
   public void testMe() {
 
-    BSimpleWithGen b = new BSimpleWithGen();
-    b.setName("blah");
+    Map<String, List<String>> map = new HashMap<>();
+    map.put("foo", new ArrayList<>());
 
-    Ebean.save(b);
+    BSimpleWithGen b = new BSimpleWithGen("blah");
+    b.setSomeMap(map);
+    DB.save(b);
 
+    final BSimpleWithGen found = DB.find(BSimpleWithGen.class, b.getId());
+    assertThat(found.getName()).isEqualTo("blah");
+    assertThat(found.getSomeMap()).isNull();
+
+    DB.delete(b);
   }
 }
