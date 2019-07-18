bfe46432dad375797041ca72ded0b046fc4ad37f
Alexander Wagner <alexander.wagner@foconis.de>
2019-07-18 12:14:38 +0200

    delete views from ddl

--- src/test/resources/dbmigration/migrationtest/h2/R__main_entity_views.sql
+++
@@ -1,8 +0,0 @@
-
-
-    create or replace view vw_main_entity as
-    select * from main_entity;
-    
-    create or replace view vw_main_entity_relation as
-    select * from main_entity_relation;
-  
