337ecef8873ad24ecb6cbc29043dedb35b701cb9
Roland Praml <praml@foconis.de>
2019-08-07 20:41:10 +0200

    FIX: Version retrieval from pom.properties did not work ENH: Check for matching ebean-agent version on startup

--- ebean-api/src/main/java/io/ebean/EbeanVersion.java
+++ ebean-api/src/main/java/io/ebean/EbeanVersion.java
@@ -17,6 +17,8 @@ public class EbeanVersion {
   private static final Logger logger = LoggerFactory.getLogger(EbeanVersion.class);
 
   private static String version = "unknown";
+  private static String requiredAgentVersion = "unknown";
+  private static String currentAgentVersion = null;
 
   static {
     try {
@@ -24,11 +26,27 @@ public class EbeanVersion {
       try (InputStream in = DB.class.getResourceAsStream("/META-INF/maven/io.ebean/ebean/pom.properties")) {
         if (in != null) {
           prop.load(in);
-          in.close();
           version = prop.getProperty("version");
+          requiredAgentVersion = prop.getProperty("agent.version");
         }
       }
-      logger.info("ebean version: {}", version);
+      try {
+        Class<?> cls = Class.forName("io.ebean.enhance.Transformer");
+        currentAgentVersion = (String) cls.getMethod("getVersion").invoke(null);
+      } catch (Exception e) {
+        logger.trace("could not get run-time-agent version", e);
+      }
+      logger.info("ebean version: {} (depends on agent {})", version, requiredAgentVersion);
+      if (currentAgentVersion != null) {
+        if (!currentAgentVersion.equals(requiredAgentVersion)) {
+          logger.error("Runtime enhancement detected. Agent version mismatch");
+          logger.error("      Current:  {}", currentAgentVersion);
+          logger.error("      Required: {}", requiredAgentVersion);
+          logger.error("THIS CAN RESULT IN UNEXPECTED BEHAVIOUR!");
+        } else {
+          logger.info("Runtime enhancement detected. Agent version matches with required version.");
+        }
+      }
     } catch (IOException e) {
       logger.warn("Could not determine ebean version: {}", e.getMessage());
     }
