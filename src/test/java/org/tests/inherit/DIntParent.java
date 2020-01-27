ebean-orm/ebean

commit a8434f22fff40e3563c5551540973cc6369b7925

Inheritance - Add support for @DiscriminatorValue to be optional (#1361)

src/main/java/io/ebeaninternal/server/deploy/parse/DeployInherit.java
@@ -126,6 +126,8 @@ public class DeployInherit {
     DiscriminatorValue dv = AnnotationUtil.findAnnotation(cls, DiscriminatorValue.class); // do not search recursive
     if (dv != null) {
       info.setDiscriminatorValue(dv.value());
+    } else {
+      info.setDiscriminatorValue(cls.getSimpleName());
     }
 
     return info;

src/main/java/io/ebeaninternal/server/deploy/parse/DeployInheritInfo.java
@@ -2,6 +2,7 @@ package io.ebeaninternal.server.deploy.parse;
 
 import io.ebeaninternal.server.deploy.InheritInfo;
 
+import java.lang.reflect.Modifier;
 import javax.persistence.DiscriminatorType;
 import java.sql.Types;
 import java.util.ArrayList;
@@ -64,7 +65,7 @@ public class DeployInheritInfo {
    * Return true if this is abstract node.
    */
   public boolean isAbstract() {
-    return (discriminatorObjectValue == null);
+    return Modifier.isAbstract(type.getModifiers());
   }
 
   /**
@@ -205,7 +206,7 @@ public class DeployInheritInfo {
   }
 
   private void appendDiscriminator(List<Object> list) {
-    if (discriminatorObjectValue != null) {
+    if (!isAbstract()) {
       list.add(discriminatorObjectValue);
     }
     for (DeployInheritInfo child : children) {

src/main/java/io/ebeaninternal/server/deploy/InheritInfo.java
@@ -6,6 +6,7 @@ import io.ebeaninternal.server.deploy.id.IdBinder;
 import io.ebeaninternal.server.deploy.parse.DeployInheritInfo;
 import io.ebeaninternal.server.query.SqlTreeProperties;
 
+import java.lang.reflect.Modifier;
 import javax.persistence.PersistenceException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -291,7 +292,7 @@ public class InheritInfo {
    * Return true if this is considered a concrete type in the inheritance hierarchy.
    */
   public boolean isConcrete() {
-    return discriminatorValue != null;
+    return !Modifier.isAbstract(type.getModifiers());
   }
 
   /**

commit df63eeaa8c9bbcb1078eb5af1d5511a4841de4bf

#1915 - Regression with DiscriminatorValue since #1361

src/main/java/io/ebeaninternal/server/deploy/parse/DeployInherit.java
@@ -128,7 +128,7 @@ public class DeployInherit {
       if (dv != null) {
         info.setDiscriminatorValue(dv.value());
       } else {
-        info.setDiscriminatorValue(cls.getSimpleName());
+        info.setDiscriminatorDefaultValue(cls);
       }
     }
     return info;

src/main/java/io/ebeaninternal/server/deploy/parse/DeployInheritInfo.java
@@ -178,6 +178,16 @@ public class DeployInheritInfo {
     return discriminatorStringValue;
   }
 
+  public void setDiscriminatorDefaultValue(Class<?> cls) {
+    if (columnType == Types.INTEGER) {
+      discriminatorStringValue = "0";
+      discriminatorObjectValue = 0;
+    } else {
+      discriminatorStringValue = cls.getSimpleName();
+      discriminatorObjectValue = discriminatorStringValue;
+    }
+  }
+
   /**
    * Set the discriminator value for this node.
    */

src/test/java/org/tests/inherit/DIntChild1.java
@@ -1,0 +1,19 @@
+package org.tests.inherit;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("1")
+public class DIntChild1 extends DIntChildBase {
+
+  @Override
+  public String getName() {
+    return "A Name";
+  }
+
+  public DIntChild1(Integer number, String more) {
+    super(number, more);
+  }
+
+}

src/test/java/org/tests/inherit/DIntChild2.java
@@ -1,0 +1,19 @@
+package org.tests.inherit;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("2")
+public class DIntChild2 extends DIntChildBase {
+
+  @Override
+  public String getName() {
+    return "Name2";
+  }
+
+  public DIntChild2(Integer number, String more) {
+    super(number, more);
+  }
+
+}

src/test/java/org/tests/inherit/DIntChildBase.java
@@ -1,0 +1,17 @@
+package org.tests.inherit;
+
+import javax.persistence.Entity;
+
+@Entity
+public class DIntChildBase extends DIntParent {
+
+  @Override
+  public String getName() {
+    return "Base name";
+  }
+
+  public DIntChildBase(Integer number, String more) {
+    super(number, more);
+  }
+
+}

src/test/java/org/tests/inherit/DIntParent.java
@@ -1,0 +1,46 @@
+package org.tests.inherit;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dint_parent")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.INTEGER)
+public abstract class DIntParent {
+
+  @Id
+  private Long id;
+
+  private Integer val;
+
+  private String more;
+
+  protected DIntParent(Integer val, String more) {
+    this.val = val;
+    this.more = more;
+  }
+
+  public abstract String getName();
+
+  public Long getId() {
+    return id;
+  }
+
+  public Integer getVal() {
+    return val;
+  }
+
+  public String getMore() {
+    return more;
+  }
+
+  public void setMore(String more) {
+    this.more = more;
+  }
+}

src/test/java/org/tests/inherit/InheritDiscIntTest.java
@@ -1,0 +1,22 @@
+package org.tests.inherit;
+
+import io.ebean.DB;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+public class InheritDiscIntTest {
+
+  @Test
+  public void test() {
+
+    DIntChild2 child2 = new DIntChild2(42, "fortyTwo");
+    DB.save(child2);
+
+    final DIntParent found = DB.find(DIntParent.class, child2.getId());
+    assertNotNull(found);
+
+    assertThat(found).isInstanceOf(DIntChild2.class);
+  }
+}