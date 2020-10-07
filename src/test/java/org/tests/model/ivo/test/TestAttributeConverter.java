bdf8381518ad93e982ddb0a368df984be9ee25d6
rob bygrave <robin.bygrave@gmail.com>
2020-10-07 14:16:55 +1300

    Update test only - TestAttributeConverter

--- src/test/java/org/tests/model/ivo/test/TestAttributeConverter.java
+++ src/test/java/org/tests/model/ivo/test/TestAttributeConverter.java
@@ -1,10 +1,14 @@
 package org.tests.model.ivo.test;
 
 import io.ebean.BaseTestCase;
-import io.ebean.Ebean;
+import io.ebean.DB;
+import org.junit.Test;
 import org.tests.model.ivo.ESomeConvertType;
 import org.tests.model.ivo.Money;
-import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestAttributeConverter extends BaseTestCase {
 
@@ -12,13 +16,26 @@ public class TestAttributeConverter extends BaseTestCase {
   public void insertUpdateDelete() {
 
     ESomeConvertType bean = new ESomeConvertType("one", new Money(20.1));
+    DB.save(bean);
 
-    Ebean.save(bean);
-
-    ESomeConvertType found = Ebean.find(ESomeConvertType.class, bean.getId());
+    ESomeConvertType found = DB.find(ESomeConvertType.class, bean.getId());
     found.setMoney(new Money("40"));
-    Ebean.save(found);
+    DB.save(found);
 
-    Ebean.delete(found);
+    final List<ESomeConvertType> list40 = DB.find(ESomeConvertType.class)
+      .where()
+      .eq("money", new Money("40"))
+      .findList();
+
+    assertThat(list40).hasSize(1);
+
+    final List<ESomeConvertType> listGt19 = DB.find(ESomeConvertType.class)
+      .where()
+      .gt("money", new Money("19"))
+      .findList();
+
+    assertThat(listGt19).hasSize(1);
+
+    DB.delete(found);
   }
 }
