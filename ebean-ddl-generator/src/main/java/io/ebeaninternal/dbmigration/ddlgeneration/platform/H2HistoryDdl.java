c9120be347aadd413371d46f974b2aa41630fa96
rbygrave <robin.bygrave@gmail.com>
2021-06-04 17:37:37 +1200

    No effective change - tidy only H2HistoryTrigger and H2HistoryDdl

--- ebean-ddl-generator/src/main/java/io/ebeaninternal/dbmigration/ddlgeneration/platform/H2HistoryDdl.java
+++ ebean-ddl-generator/src/main/java/io/ebeaninternal/dbmigration/ddlgeneration/platform/H2HistoryDdl.java
@@ -1,6 +1,5 @@
 package io.ebeaninternal.dbmigration.ddlgeneration.platform;
 
-import io.ebean.config.dbplatform.h2.H2HistoryTrigger;
 import io.ebeaninternal.dbmigration.ddlgeneration.DdlBuffer;
 import io.ebeaninternal.dbmigration.ddlgeneration.DdlWrite;
 import io.ebeaninternal.dbmigration.model.MTable;
@@ -22,31 +21,25 @@ public class H2HistoryDdl extends DbTriggerBasedHistoryDdl {
 
   @Override
   protected void dropTriggers(DdlBuffer buffer, String baseTable) throws IOException {
-
     buffer.append("drop trigger ").append(updateTriggerName(baseTable)).endOfStatement();
   }
 
   @Override
   protected void createTriggers(DdlWrite writer, MTable table) throws IOException {
-
     String baseTableName = table.getName();
     DdlBuffer apply = writer.applyHistoryTrigger();
-
     addCreateTrigger(apply, updateTriggerName(baseTableName), baseTableName);
   }
 
   @Override
   protected void updateHistoryTriggers(DbTriggerUpdate update) throws IOException {
-
     recreateHistoryView(update);
-
     DdlBuffer buffer = update.historyTriggerBuffer();
     dropTriggers(buffer, update.getBaseTable());
     addCreateTrigger(buffer, updateTriggerName(update.getBaseTable()), update.getBaseTable());
   }
 
   private void addCreateTrigger(DdlBuffer apply, String triggerName, String baseTable) throws IOException {
-
     // Note that this does not take into account the historyTable name (excepts _history suffix) and
     // does not take into account excluded columns (all columns included in history)
     apply
