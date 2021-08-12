531024b8b4632525edbac4ea6b3a83434d0d9d6e
Thomas Fellner <thomas.fellner@foconis.de>
2021-08-12 15:47:21 +0200

    Fix test TestGeneratedKeys

--- ebean-core/src/test/java/org/tests/idkeys/db/GenKeySequence.java
+++ ebean-core/src/test/java/org/tests/idkeys/db/GenKeySequence.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class GenKeySequence {
   public final static String SEQUENCE_NAME = "SEQ";
 
   @Id
+  @SequenceGenerator(name = "SEQ_NAME", sequenceName = GenKeySequence.SEQUENCE_NAME)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_NAME")
   private Long id;
 
