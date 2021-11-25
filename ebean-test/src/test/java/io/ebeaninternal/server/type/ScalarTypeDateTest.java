21847177ebe7d9e6b3d49fa9cd82ca0dcddf0baa
Roland Praml <roland.praml@foconis.de>
2021-11-25 16:58:17 +0100

    Fix: ScalarTypeDateTest

--- ebean-test/src/test/java/io/ebeaninternal/server/type/ScalarTypeDateTest.java
+++ ebean-test/src/test/java/io/ebeaninternal/server/type/ScalarTypeDateTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,6 +18,8 @@ public class ScalarTypeDateTest {
   @Test
   public void formatParse_PG_DATE_POSITIVE_INFINITY() {
 
+    // Note: This test does not pass if 
+    // TimeZone.setDefault(TimeZone.getTimeZone("PST"));
     Date postgresInfinityDate = new Date(9223372036825200000L);
 
     String format = type.formatValue(postgresInfinityDate);
@@ -26,8 +30,9 @@ public class ScalarTypeDateTest {
   @Test
   public void json() throws IOException {
 
-
-    Date val = new Date(1557360000000L);
+    // Date val = new Date(1557360000000L); does not work if
+    // TimeZone.setDefault(TimeZone.getTimeZone("PST"));
+    Date val = Date.valueOf(LocalDate.of(2019,5,9)); // erase tz dependent fraction.
 
     JsonTester<Date> jsonMillis = new JsonTester<>(type);
     assertThat(jsonMillis.test(val)).isEqualTo("{\"key\":1557360000000}");
