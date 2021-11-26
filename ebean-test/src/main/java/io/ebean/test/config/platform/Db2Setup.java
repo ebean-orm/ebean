dfb1d6ab54b35a807b7cf429d8c7e6805b8dcdec
Roland Praml <roland.praml@foconis.de>
2021-11-26 15:01:15 +0100

    ADD: Db2 container support

---
+++ ebean-test/src/main/java/io/ebean/test/config/platform/Db2Setup.java
@@ -0,0 +1,41 @@
+package io.ebean.test.config.platform;
+
+import java.util.Properties;
+
+class Db2Setup implements PlatformSetup {
+
+  @Override
+  public Properties setup(Config config) {
+
+    config.ddlMode("dropCreate");
+    config.setDefaultPort(50000);
+    config.setUsernameDefault();
+    config.setPasswordDefault();
+    config.setUrl("jdbc:db2://localhost:${port}/${databaseName}");
+    config.setDriver("com.ibm.db2.jcc.DB2Driver");
+    config.datasourceDefaults();
+
+    return dockerProperties(config);
+  }
+
+  private Properties dockerProperties(Config dbConfig) {
+
+    if (!dbConfig.isUseDocker()) {
+      return new Properties();
+    }
+
+    dbConfig.setDockerVersion("11.5.6.0a");
+    return dbConfig.getDockerProperties();
+  }
+
+  @Override
+  public void setupExtraDbDataSource(Config config) {
+    // not supported yet
+  }
+
+  @Override
+  public boolean isLocal() {
+    return false;
+  }
+
+}
