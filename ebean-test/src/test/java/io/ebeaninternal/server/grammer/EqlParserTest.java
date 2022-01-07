3fd746cd3600cb6795c23c74d0cac1f56d2b344a
Roland Praml <roland.praml@foconis.de>
2022-01-07 13:00:43 +0100

    FIX: Compile error on EqlParserTest

--- ebean-test/src/test/java/io/ebeaninternal/server/grammer/EqlParserTest.java
+++ ebean-test/src/test/java/io/ebeaninternal/server/grammer/EqlParserTest.java
@@ -261,7 +261,7 @@ public class EqlParserTest extends BaseTestCase {
   }
 
   @Test
-  @ForPlatform({Platform.HANA, Platform.DB2}))
+  @ForPlatform({Platform.HANA, Platform.DB2})
   public void where_or2_hana() {
 
     Query<Customer> query = parse("where (name = 'Rob' or status = 'N') and smallnote is null");
@@ -271,7 +271,7 @@ public class EqlParserTest extends BaseTestCase {
   }
 
   @Test
-  @IgnorePlatform({Platform.HANA, Platform.DB2})) // The HANA & DB2 JDBC driver checks the field length on binding and rejects 'NEW'
+  @IgnorePlatform({Platform.HANA, Platform.DB2}) // The HANA & DB2 JDBC driver checks the field length on binding and rejects 'NEW'
   public void test_simplifyExpressions() {
 
     Query<Customer> query = parse("where not (name = 'Rob' and status = 'NEW')");
