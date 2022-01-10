8d85000970df57434d1b1a9b5bb6dbe89dba15dc
FOCONIS\szemenyei <noemi.szemenyei@foconis.de>
2022-01-10 15:55:58 +0100

    Fix Checksum calculate() with special characters
    (cherry picked from commit e9efd5a70222f372a38d5bcac4254de3759010b9)

--- ebean-ddl-generator/src/main/java/io/ebeaninternal/dbmigration/MChecksum.java
+++ ebean-ddl-generator/src/main/java/io/ebeaninternal/dbmigration/MChecksum.java
@@ -1,6 +1,7 @@
 package io.ebeaninternal.dbmigration;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.CRC32;
 
@@ -13,9 +14,8 @@ class MChecksum {
    * Returns the checksum of the file. Agnostic of encoding and new line character.
    */
   static int calculate(File file) {
-    try {
+    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("UTF-8")))) {
       final CRC32 crc32 = new CRC32();
-      BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
       String line;
       while ((line = bufferedReader.readLine()) != null) {
         final byte[] lineBytes = line.getBytes(StandardCharsets.UTF_8);
