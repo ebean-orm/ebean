88ab852855cb96092eaaae478e257385331e43d6
Rob Bygrave <robin.bygrave@gmail.com>
2022-01-17 21:28:45 +1300

    No real change, tidy DbExpressionHandlerFactory only

--- ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/DbExpressionHandlerFactory.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/DbExpressionHandlerFactory.java
@@ -10,23 +10,18 @@ public final class DbExpressionHandlerFactory {
    * Create and return the appropriate platform specific handing of expressions.
    */
   public static DbExpressionHandler from(DatabasePlatform databasePlatform) {
-    Platform platform = databasePlatform.getPlatform();
+    Platform platform = databasePlatform.getPlatform().base();
     switch (platform) {
       case H2:
         return new H2DbExpression();
       case POSTGRES:
-      case POSTGRES9:
         return new PostgresDbExpression();
       case MARIADB:
         return new MariaDbExpression();
-      case MYSQL55:
       case MYSQL:
         return new MySqlDbExpression();
       case ORACLE:
-      case ORACLE11:
         return new OracleDbExpression();
-      case SQLSERVER16:
-      case SQLSERVER17:
       case SQLSERVER:
         return new SqlServerDbExpression();
       case HANA:
