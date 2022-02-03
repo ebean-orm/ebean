0442bd07042900156383023479a2d634d0d7d5af
Roland Praml <roland.praml@foconis.de>
2022-02-03 09:31:29 +0100

    Disabled test for github

--- ebean-test/src/test/java/io/ebeaninternal/dbmigration/ServerStartTest.java
+++ ebean-test/src/test/java/io/ebeaninternal/dbmigration/ServerStartTest.java
@@ -1,23 +1,15 @@
 package io.ebeaninternal.dbmigration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.ebean.DatabaseFactory;
-import io.ebean.config.DatabaseConfig;
 
 public class ServerStartTest {
 
-  
   @Test
-  void testServerStartAndMigrate() throws Exception {
+  @Disabled("run manually")
+  void testServerStartAndMigrateDb2() throws Exception {
     DatabaseFactory.create("db2-migration").shutdown();
   }
 }
