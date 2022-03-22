de4140aaaca81def3d92d30cd39222c06ef2384e
Rob Bygrave <robin.bygrave@gmail.com>
2022-03-22 16:20:06 +1300

    #2600 - Pending drops migration with the same version of the target migration cause trouble

--- ebean-ddl-generator/src/test/java/io/ebeaninternal/dbmigration/DefaultDbMigrationTest.java
+++ ebean-ddl-generator/src/test/java/io/ebeaninternal/dbmigration/DefaultDbMigrationTest.java
@@ -3,18 +3,31 @@ package io.ebeaninternal.dbmigration;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class DefaultDbMigrationTest {
+class DefaultDbMigrationTest {
 
   private final DefaultDbMigration migration = new DefaultDbMigration();
 
   @Test
-  public void trimDropsFor() {
+  void trimDropsFor() {
     assertEquals("1.2", migration.trimDropsFor("V1.2__hello"));
     assertEquals("1.2", migration.trimDropsFor("v1.2__hello"));
     assertEquals("1.2", migration.trimDropsFor("v1.2"));
     assertEquals("junk1.2", migration.trimDropsFor("junk1.2"));
     assertEquals("junk1.2", migration.trimDropsFor("junk1.2__"));
     assertEquals("junk1.2", migration.trimDropsFor("junk1.2__more"));
+  }
+
+  @Test
+  void checkDropVersion_when_matches_throwsIAE() {
+    assertThrows(IllegalArgumentException.class, () -> migration.checkDropVersion("1.0", "1.0"));
+  }
+
+  @Test
+  void checkDropVersion_ok() {
+    migration.checkDropVersion("1.0", null);
+    migration.checkDropVersion("1.0", "1.0.0");
+    migration.checkDropVersion("1.0", "1.1");
   }
 }
