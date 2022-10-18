c855df8b8d6f707b11da03de2126abc6e5901581
Noemi Praml <noemi.praml@foconis.de>
2022-10-18 07:59:19 +0200

    javadoc

--- ebean-api/src/main/java/io/ebean/config/BackgroundExecutorWrapper.java
+++ ebean-api/src/main/java/io/ebean/config/BackgroundExecutorWrapper.java
@@ -6,6 +6,10 @@ import java.util.concurrent.Callable;
  * BackgroundExecutorWrapper that can be used to wrap tasks that are sent to background (i.e. another thread).
  * It should copy all necessary thread-local variables. See {@link MdcBackgroundExecutorWrapper} for implementation details.
  *
+ * Note: only tasks that are executed immediately (submit, execute) are wrapped. Periodic or scheduled tasks are not wrapped,
+ * as these may keep copied variables in memory either forever or until the scheduled task is finished.
+ * The caller is responsible to handle these cases.
+ *
  * @author Roland Praml, FOCONIS AG
  */
 public interface BackgroundExecutorWrapper {
