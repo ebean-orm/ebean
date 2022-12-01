95512d485139730cec7342dbbcc4d048216f9836
Rob Bygrave <robin.bygrave@gmail.com>
2022-12-01 22:27:57 +1300

    #2906 - Tidy LimitOffsetSqlLimiter

--- ebean-api/src/main/java/io/ebean/config/dbplatform/LimitOffsetSqlLimiter.java
+++ ebean-api/src/main/java/io/ebean/config/dbplatform/LimitOffsetSqlLimiter.java
@@ -5,16 +5,6 @@ package io.ebean.config.dbplatform;
  */
 public final class LimitOffsetSqlLimiter implements SqlLimiter {
 
-  /**
-   * LIMIT keyword.
-   */
-  private static final String LIMIT = "limit";
-
-  /**
-   * OFFSET keyword.
-   */
-  private static final String OFFSET = "offset";
-
   @Override
   public SqlLimitResponse limit(SqlLimitRequest request) {
     String dbSql = request.getDbSql();
@@ -26,12 +16,11 @@ public final class LimitOffsetSqlLimiter implements SqlLimiter {
     sb.append(dbSql);
     int maxRows = request.getMaxRows();
     if (maxRows > 0) {
-      sb.append(" ").append(LIMIT).append(" ").append(maxRows);
+      sb.append(" limit ").append(maxRows);
     }
     int firstRow = request.getFirstRow();
     if (firstRow > 0) {
-      sb.append(" ").append(OFFSET).append(" ");
-      sb.append(firstRow);
+      sb.append(" offset ").append(firstRow);
     }
     String sql = request.getDbPlatform().completeSql(sb.toString(), request.getOrmQuery());
     return new SqlLimitResponse(sql);
