4e2a2ef1d20a6e07771d2177cad38cefb3d10529
Roland Praml <roland.praml@foconis.de>
2023-01-23 13:23:23 +0100

    Some ISO8601 fixes in joda time

--- ebean-joda-time/src/test/java/io/ebean/joda/time/ScalarTypeJodaLocalDateTimeTest.java
+++ ebean-joda-time/src/test/java/io/ebean/joda/time/ScalarTypeJodaLocalDateTimeTest.java
@@ -1,10 +1,15 @@
 package io.ebean.joda.time;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import io.ebean.config.JsonConfig;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ScalarTypeJodaLocalDateTimeTest {
 
   ScalarTypeJodaLocalDateTime type = new ScalarTypeJodaLocalDateTime(JsonConfig.DateTime.ISO8601);
+  JsonFactory jsonFactory = new JsonFactory();
 
   @Test
   public void testConvertFromTimestamp() throws Exception {
@@ -30,6 +36,17 @@ public class ScalarTypeJodaLocalDateTimeTest {
     assertEquals(ts1, ts2);
   }
 
+  @Test
+  void testIsoFormat() throws IOException {
+    long millis = 1674472451640L;
+    JsonParser parser = jsonFactory.createParser(new StringReader("\"2023-01-23T12:14:11.640\""));
+    JsonToken token = parser.nextToken();
+    assertEquals(JsonToken.VALUE_STRING, token);
+
+    LocalDateTime ldt1 = type.jsonRead(parser);
+    String iso = type.toJsonISO8601(ldt1);
+    assertEquals("2023-01-23T12:14:11.640", iso);
+  }
 
   LocalDateTime localConvertFromTimestamp(Timestamp ts) {
     return new LocalDateTime(ts.getTime(), DateTimeZone.getDefault());
