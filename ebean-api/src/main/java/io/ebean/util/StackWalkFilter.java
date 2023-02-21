b0364f3c42050a3fc3ef60883ab95888bad93578
rob <robin.bygrave@gmail.com>
2023-02-21 14:21:18 +1300

    Use StackWalker for ProfileLocation and CallOrigin

---
+++ ebean-api/src/main/java/io/ebean/util/StackWalkFilter.java
@@ -0,0 +1,28 @@
+package io.ebean.util;
+
+import java.util.function.Predicate;
+
+/**
+ * Provides a stack filter that excludes ebean and jdk code.
+ */
+public final class StackWalkFilter {
+
+  private static final Filter FILTER = new Filter();
+
+  /**
+   * Return a stack filter that excludes ebean and jdk code.
+   */
+  public static Predicate<StackWalker.StackFrame> filter() {
+    return FILTER;
+  }
+
+  private static class Filter implements Predicate<StackWalker.StackFrame> {
+
+    @Override
+    public boolean test(StackWalker.StackFrame stackFrame) {
+      return !stackFrame.getClassName().startsWith("io.ebean")
+        && !stackFrame.getClassName().startsWith("jdk.")
+        && !stackFrame.getMethodName().startsWith("_ebean_");
+    }
+  }
+}
