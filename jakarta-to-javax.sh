8ea0936da8431fcbb36654a578c396b30a003a34
rob <robin.bygrave@gmail.com>
2023-02-23 20:43:50 +1300

    Tighten Javax to Jakarta escaping the periods

--- jakarta-to-javax.sh
+++ jakarta-to-javax.sh
@@ -9,16 +9,16 @@ find . -type f -name 'pom.xml' -exec sed -i'' -e 's|____ JAVAX-DEPENDENCY-END --
 sed -i'' -e 's|artifactId>jakarta-persistence-api|artifactId>persistence-api|g' ebean-api/pom.xml ebean-bom/pom.xml
 
 ## adjust module-info
-sed -i'' -e 's| jakarta.persistence.api| persistence.api|g' ebean-api/src/main/java/module-info.java
-sed -i'' -e 's| jakarta.servlet;| javax.servlet.api;|g' ebean-api/src/main/java/module-info.java
+sed -i'' -e 's| jakarta\.persistence\.api| persistence\.api|g' ebean-api/src/main/java/module-info.java
+sed -i'' -e 's| jakarta\.servlet;| javax\.servlet\.api;|g' ebean-api/src/main/java/module-info.java
 
-find . -type f -name 'module-info.java' -exec sed -i'' -e 's|jakarta.xml.bind|java.xml.bind|g' {} +
+find . -type f -name 'module-info.java' -exec sed -i'' -e 's|jakarta\.xml\.bind|java\.xml\.bind|g' {} +
 
 ## adjust code
-find ebean-api/src/main/java/io/ebean/event -type f -name '*.java' -exec sed -i'' -e 's|jakarta.servlet|javax.servlet|g' {} +
+find ebean-api/src/main/java/io/ebean/event -type f -name '*.java' -exec sed -i'' -e 's|jakarta\.servlet|javax\.servlet|g' {} +
 
-find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta.persistence.|javax.persistence.|g' {} +
-find . -type f -name '*.kt' -exec sed -i'' -e 's|jakarta.persistence.|javax.persistence.|g' {} +
-find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta.xml.bind|javax.xml.bind|g' {} +
-find . -type f -name '*.kt' -exec sed -i'' -e 's|jakarta.xml.bind|javax.xml.bind|g' {} +
+find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta\.persistence\.|javax\.persistence\.|g' {} +
+find . -type f -name '*.kt' -exec sed -i'' -e 's|jakarta\.persistence\.|javax\.persistence\.|g' {} +
+find . -type f -name '*.java' -exec sed -i'' -e 's|jakarta\.xml\.bind|javax\.xml\.bind|g' {} +
+find . -type f -name '*.kt' -exec sed -i'' -e 's|jakarta\.xml\.bind|javax\.xml\.bind|g' {} +
 
