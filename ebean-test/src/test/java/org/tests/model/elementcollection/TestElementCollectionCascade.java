efcee7e739880d6379800b142916614ba94c2129
Rob Bygrave <robin.bygrave@gmail.com>
2023-03-10 21:33:58 +1300

    Improve test asserts for lazy loading element collection

--- ebean-test/src/test/java/org/tests/model/elementcollection/TestElementCollectionCascade.java
+++ ebean-test/src/test/java/org/tests/model/elementcollection/TestElementCollectionCascade.java
@@ -1,7 +1,8 @@
 package org.tests.model.elementcollection;
 
-import io.ebean.xtest.BaseTestCase;
 import io.ebean.DB;
+import io.ebean.test.LoggedSql;
+import io.ebean.xtest.BaseTestCase;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
@@ -32,12 +33,20 @@ public class TestElementCollectionCascade extends BaseTestCase {
 
     Map<String, Set<String>> childVals = new LinkedHashMap<>();
 
+    LoggedSql.start();
     final EcsmParent foundParent = DB.find(EcsmParent.class, parent.getId());
     final List<EcsmChild> children1 = foundParent.getChildren();
     for (EcsmChild ecsmOne : children1) {
       final Set<String> values = ecsmOne.getValues();
       values.size();
       childVals.put(ecsmOne.getName(), values);
+    }
+    List<String> sql = LoggedSql.stop();
+    assertThat(sql).hasSize(3);
+    assertThat(sql.get(0)).contains("select t0.id, t0.name, t0.version from ecsm_parent t0 where t0.id = ?");
+    if (!isPostgresCompatible()) {
+      assertThat(sql.get(1)).contains("select t0.ecsm_parent_id, t0.one_id, t0.name, t0.version from ecsm_child t0 where (t0.ecsm_parent_id) in (?)");
+      assertThat(sql.get(2)).contains("select t0.host_id, t0.value from ecsm_values t0 where (t0.host_id) in (?,?)");
     }
 
     Set<String> vals0 = childVals.get("c0");
