bf92e103bc20a6881f219337c9f0c9965be1adee
Roland Praml <praml@foconis.de>
2023-03-17 10:38:52 +0100

    FIX: DB.json().toBean(target) can update existing lists (#85)
    * FIX: DB.json().toBean(target) can update existing lists
    
    * remove sys.print, typo
    
    ---------
    
    Co-authored-by: Roland Praml <roland.praml@foconis.de>
    Co-authored-by: Juri Skrobko <juri.skrobko@foconis.de>

--- ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanPropertyAssocOne.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanPropertyAssocOne.java
@@ -811,14 +811,20 @@ public class BeanPropertyAssocOne<T> extends BeanPropertyAssoc<T> implements STr
   @Override
   public void jsonRead(SpiJsonReader readJson, EntityBean bean) throws IOException {
     if (jsonDeserialize && targetDescriptor != null) {
-      T target = (T) value(bean);
+      // CHECKME: may we skip reading the object from the json stream?
+      T target = readJson.update() ? (T) value(bean) : null;
       T assocBean = targetDescriptor.jsonRead(readJson, name, target);
-      if (readJson.intercept()) {
+      if (readJson.update()) {
         setValueIntercept(bean, assocBean);
       } else {
         setValue(bean, assocBean);
       }
     }
+  }
+
+  @Override
+  public Object jsonRead(SpiJsonReader readJson) throws IOException {
+    return targetDescriptor.jsonRead(readJson, name, null);
   }
 
   public boolean isReference(Object detailBean) {
