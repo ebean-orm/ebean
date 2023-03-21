1687186ad82f781d50ecce7bb2f31afb493cdb58
Michal Buchtík <xbuchtajz@gmail.com>
2023-03-21 16:21:45 +0100

    failing test for Where raw clause subquery

--- ebean-test/src/test/java/org/tests/query/TestWhereRawClause.java
+++ ebean-test/src/test/java/org/tests/query/TestWhereRawClause.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestWhereRawClause extends BaseTestCase {
 
@@ -186,5 +187,57 @@ public class TestWhereRawClause extends BaseTestCase {
       .raw("unitPrice > ? and product.id > ?", 2, 3)
       .findList();
 
+  }
+
+  @Test
+  public void testRawClauseSubQuery() {
+    LoggedSql.start();
+
+    List<Customer> list;
+    List<String> sql;
+
+    DB.find(Customer.class)
+      .select("name")
+      .where()
+      .raw("id in (select c.id as test_id from o_customer c)")
+      .findList();
+
+    sql = LoggedSql.collect();
+    System.out.println(sql);
+    assertThat(sql.get(0)).contains("select t0.id, t0.name from o_customer t0 where t0.id in (select c.id as test_id from o_customer c);");
+
+
+    /*
+    this generate wrong query and throw SQLException. This is bug not only for column id, but others too (when there is conflict with name from upper level table)
+
+    Syntax error in SQL statement "select t0.id, t0.name from o_customer t0 where t0.id in (select c.id as t0[*].id from o_customer c)"; expected ",, FROM, WHERE, GROUP BY, HAVING, WINDOW, QUALIFY, INTERSECT, UNION, EXCEPT, MINUS, ORDER BY, OFFSET, FETCH, LIMIT, FOR, ,, )";
+    SQL statement:
+    select t0.id, t0.name from o_customer t0 where t0.id in (select c.id as t0.id from o_customer c) [42001-214] Bind values:[null] Query was:select t0.id, t0.name from o_customer t0 where t0.id in (select c.id as t0.id from o_customer c)
+    */
+    DB.find(Customer.class)
+      .select("name")
+      .where()
+      .raw("id in (select c.id as id from o_customer c)")
+      .findList();
+
+    sql = LoggedSql.collect();
+    System.out.println(sql);
+    assertThat(sql.get(0)).contains("select t0.id, t0.name from o_customer t0 where t0.id in (select c.id as id from o_customer c);");
+
+
+    /*
+    example of possible workaround
+    subqueries are created by users, so we cannot use only one possible way to keep it working
+     */
+
+    DB.find(Customer.class)
+      .select("name")
+      .where()
+      .raw("id in (select \"id\" from (select c.id as \"id\", c.id || ' ' || c.name as \"name\" from o_customer c) w(\"id\", \"name\") )")
+      .findList();
+
+    sql = LoggedSql.stop();
+    System.out.println(sql);
+    assertThat(sql.get(0)).contains("select t0.id, t0.name from o_customer t0 where t0.id in (select \"id\" from (select c.id as \"id\", c.id || ' ' || c.name as \"name\" from o_customer c) w(\"id\", \"name\") );");
   }
 }
