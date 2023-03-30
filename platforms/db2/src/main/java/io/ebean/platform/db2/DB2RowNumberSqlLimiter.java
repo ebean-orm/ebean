1807fc201e294b661dc5abe722a1f9d7e200e660
Rob Bygrave <robin.bygrave@gmail.com>
2023-03-30 22:56:55 +1300

    Initial DB2Luw9Platform using row_number() for row limiting sql

---
+++ platforms/db2/src/main/java/io/ebean/platform/db2/DB2RowNumberSqlLimiter.java
@@ -0,0 +1,38 @@
+package io.ebean.platform.db2;
+
+import io.ebean.config.dbplatform.SqlLimitRequest;
+import io.ebean.config.dbplatform.SqlLimitResponse;
+import io.ebean.config.dbplatform.SqlLimiter;
+
+
+final class DB2RowNumberSqlLimiter implements SqlLimiter {
+
+  @Override
+  public SqlLimitResponse limit(SqlLimitRequest request) {
+    String dbSql = request.getDbSql();
+
+    StringBuilder sb = new StringBuilder(60 + dbSql.length());
+    int firstRow = request.getFirstRow();
+    int lastRow = request.getMaxRows();
+    if (lastRow > 0) {
+      lastRow += firstRow;
+    }
+
+    sb.append("select * from (select row_number() over() rn, a.* from (select ");
+    if (request.isDistinct()) {
+      sb.append("distinct ");
+    }
+    sb.append(dbSql).append(") a ");
+    if (lastRow > 0) {
+      sb.append(" where rn <= ").append(lastRow);
+    }
+    sb.append(") ");
+    if (firstRow > 0) {
+      sb.append(" where rn > ").append(firstRow);
+    }
+
+    String sql = request.getDbPlatform().completeSql(sb.toString(), request.getOrmQuery());
+    return new SqlLimitResponse(sql);
+  }
+
+}
