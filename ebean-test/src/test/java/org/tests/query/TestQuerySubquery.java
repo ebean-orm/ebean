4a6003027b32d9356375e3e2d4d1a699cb258603
Jonas Pöhler <jonas.poehler@foconis.de>
2023-08-21 11:50:09 +0200

    PR #3180 - Using a subquery with idEq or setId is missing the bind value

---
+++ ebean-test/src/test/java/org/tests/query/TestQuerySubquery.java
@@ -0,0 +1,111 @@
+package org.tests.query;
+
+import io.ebean.DB;
+import io.ebean.test.LoggedSql;
+import io.ebean.xtest.BaseTestCase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.tests.model.basic.Order;
+import org.tests.model.basic.Person;
+import org.tests.model.basic.ResetBasicData;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestQuerySubquery extends BaseTestCase {
+
+  private Long robId;
+
+  @BeforeEach
+  public void setup() {
+    ResetBasicData.reset();
+
+    Person rob = new Person();
+    rob.setName("Rob");
+    rob.setSurname("Test");
+    DB.save(rob);
+    robId = rob.getId();
+
+    DB.getDefault().pluginApi().cacheManager().clearAll();
+  }
+
+  @AfterEach
+  public void cleanup() {
+    DB.delete(Person.class, robId);
+  }
+
+  @Test
+  public void testWithoutSubquery() {
+
+    Person contact = DB.find(Person.class)
+      .where()
+      .idEq(robId)
+      .findOne();
+
+    LoggedSql.start();
+    List<Order> orders = DB.find(Order.class)
+      .where()
+      .eq("customer.name", contact.getName())
+      .findList();
+
+    assertThat(orders).hasSize(3);
+    assertThat(LoggedSql.stop())
+      .hasSize(1)
+      .first().asString()
+      .contains("--bind(" + contact.getName() + ")")
+      .contains("kcustomer_id where t1.name = ?");
+  }
+
+  @Test
+  public void testEqSubqueryWithIdEq() {
+    LoggedSql.start();
+    List<Order> orders = DB.find(Order.class)
+      .where()
+      .eq("customer.name", DB.find(Person.class).alias("sq").select("name").where().idEq(robId).query())
+      .findList();
+
+    assertThat(orders).hasSize(3);
+    assertThat(LoggedSql.stop())
+      .hasSize(1)
+      .first().asString()
+      .contains("--bind(" + robId + ")")
+      .contains("t1.name = (select sq.NAME from PERSONS sq where sq.ID = ?)");
+  }
+
+  @Test
+  public void testEqSubqueryWithSetId() {
+    LoggedSql.start();
+
+    List<Order> orders = DB.find(Order.class)
+      .where()
+      .eq("customer.name", DB.find(Person.class).alias("sq").select("name").setId(robId))
+      .findList();
+
+    assertThat(orders).hasSize(3);
+    assertThat(LoggedSql.stop())
+      .hasSize(1)
+      .first().asString()
+      .contains("--bind(" + robId + ")")
+      .contains("t1.name = (select sq.NAME from PERSONS sq where sq.ID = ?)");
+  }
+
+  @Test
+  public void testEqSubqueryWithEqId() {
+    LoggedSql.start();
+
+    List<Order> orders = DB.find(Order.class)
+      .where()
+      .eq("customer.name", DB.find(Person.class).alias("sq").select("name").where().eq("id", robId).query())
+      .findList();
+
+    assertThat(orders).hasSize(3);
+    assertThat(LoggedSql.stop())
+      .hasSize(1)
+      .first().asString()
+      .contains("--bind(" + robId + ")")
+      .contains("t1.name = (select sq.NAME from PERSONS sq where sq.ID = ?)");
+  }
+
+}
