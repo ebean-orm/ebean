04ba81d12f7294f7d15f22c2da0e452639843800
Rob Bygrave <robin.bygrave@gmail.com>
2024-02-09 08:30:42 +1300

    #3310 Update test only, reuse existing entity beans
    - Reuse the existing OmBeanListParent and child
    - Simplify the test setup code
    - Rename test methods to maybe better reflect what I think is failing
    
    Noting that markAsDirty doesn't specifically have anything to do with this bug but it's more on whether the parent bean is dirty or dirty (markAsDirty is just a way to make the parent bean dirty).

--- ebean-test/src/test/java/org/tests/o2m/OneToManyListMarkAsDirtyTest.java
+++ ebean-test/src/test/java/org/tests/o2m/OneToManyListMarkAsDirtyTest.java
@@ -3,147 +3,106 @@ package org.tests.o2m;
 import io.ebean.DB;
 import io.ebean.xtest.BaseTestCase;
 import org.junit.jupiter.api.Test;
-import org.tests.o2m.dm.*;
-
-import java.sql.Timestamp;
+import org.tests.model.orphanremoval.OmBeanListChild;
+import org.tests.model.orphanremoval.OmBeanListParent;
+import java.time.Instant;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OneToManyListMarkAsDirtyTest extends BaseTestCase {
+class OneToManyListMarkAsDirtyTest extends BaseTestCase {
   @Test
-  public void workingTestCase() {
-    var g = new GoodsEntity();
-    DB.save(g);
+  void usingNewListWithNonDirtyParent_expect_orphanDeleted_works() {
+    // setup
+    var parent = new OmBeanListParent();
+    var a_b = new OmBeanListChild("b");
+    parent.getChildren().add(a_b);
+    DB.save(parent);
 
-    var planStart = new StrategicPlan();
-    planStart.setName("aaa");
-    planStart.setGoodsCapacities(new ArrayList<>());
-    DB.save(planStart);
+    // act
+    var secondParent = DB.find(OmBeanListParent.class, parent.getId());
+    secondParent.setChildren2(new ArrayList<>()); // <!-- HERE - setting a new ArrayList rather than using clear()
+    var a_c = new OmBeanListChild("b");
+    secondParent.getChildren().add(a_c);
+    // working here as secondParent itself is not dirty
+    DB.save(secondParent);
 
-    var planFirstCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planFirstCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_b = new GoodsCapacity();
-    a_b.setGoods(g);
-    planFirstCapacity.getGoodsCapacities().add(a_b);
-    DB.save(planFirstCapacity);
-
-    var planSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planSecondCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_c = new GoodsCapacity();
-    a_c.setGoods(g);
-    planSecondCapacity.getGoodsCapacities().add(a_c);
-    // without this the goods capacity is saved correctly, version remains same
-    // (not sure if version should stay the same if OneToMany relationship is changed (through cascade)
-    // DB.markAsDirty(planSecondCapacity);
-    DB.save(planSecondCapacity);
-
-    var refreshedPlanSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().size()).isEqualTo(1);
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().get(0).getId()).isEqualTo(a_c.getId());
-    // todo is this expected?
-    assertThat(refreshedPlanSecondCapacity.getVersion()).isEqualTo(planFirstCapacity.getVersion());
-    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isEqualTo(planFirstCapacity.getWhenModified());
+    var refreshedPlanSecondCapacity = DB.find(OmBeanListParent.class, parent.getId());
+    assertThat(refreshedPlanSecondCapacity.getChildren().size()).isEqualTo(1);
+    assertThat(refreshedPlanSecondCapacity.getChildren().get(0).getId()).isEqualTo(a_c.getId());
+    // Q: is this expected? A: Rob Bygrave - Yes it is expected
+    assertThat(refreshedPlanSecondCapacity.getVersion()).isEqualTo(parent.getVersion());
+    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isEqualTo(parent.getWhenModified());
   }
 
   @Test
-  public void oneToManyShouldBeDeletedWithParentMarkAsDirtyTest() {
-    var g = new GoodsEntity();
-    DB.save(g);
+  void usingNewListWithParentDirty_expect_orphanDeleted_fails2() {
+    // setup
+    var parent = new OmBeanListParent();
+    var a_b = new OmBeanListChild("b");
+    parent.getChildren().add(a_b);
+    DB.save(parent);
 
-    var planStart = new StrategicPlan();
-    planStart.setName("aaa");
-    planStart.setGoodsCapacities(new ArrayList<>());
-    DB.save(planStart);
-
-    var planFirstCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planFirstCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_b = new GoodsCapacity();
-    a_b.setGoods(g);
-    planFirstCapacity.getGoodsCapacities().add(a_b);
-    DB.save(planFirstCapacity);
-
-    var planSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planSecondCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_c = new GoodsCapacity();
-    a_c.setGoods(g);
-    planSecondCapacity.getGoodsCapacities().add(a_c);
+    // act
+    var secondParent = DB.find(OmBeanListParent.class, parent.getId());
+    secondParent.setChildren2(new ArrayList<>()); // <!-- HERE: using new ArrayList with orphanRemoval
+    var a_c = new OmBeanListChild("c");
+    secondParent.getChildren().add(a_c);
     // force version increase
-    // without this the goods capacity is saved correctly, but version isn't
-    DB.markAsDirty(planSecondCapacity);
-    DB.save(planSecondCapacity);
+    DB.markAsDirty(secondParent);
+    DB.save(secondParent);
 
-    var refreshedPlanSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().size()).isEqualTo(1);
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().get(0).getId()).isEqualTo(a_c.getId());
-    assertThat(refreshedPlanSecondCapacity.getVersion()).isGreaterThan(planFirstCapacity.getVersion());
-    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isAfter(planFirstCapacity.getWhenModified());
+    var refreshedPlanSecondCapacity = DB.find(OmBeanListParent.class, parent.getId());
+    assertThat(refreshedPlanSecondCapacity.getChildren().size()).isEqualTo(1);
+    assertThat(refreshedPlanSecondCapacity.getChildren().get(0).getId()).isEqualTo(a_c.getId());
+    assertThat(refreshedPlanSecondCapacity.getVersion()).isGreaterThan(parent.getVersion());
+    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isAfter(parent.getWhenModified());
   }
 
   @Test
-  public void oneToManyShouldBeDeletedWithParentMarkAsDirtyTestWorkAround() {
-    var g = new GoodsEntity();
-    DB.save(g);
+  void usingClearWithParentDirty_expect_orphanDeleted_works() {
+    // setup
+    var parent = new OmBeanListParent();
+    var a_b = new OmBeanListChild("b");
+    parent.getChildren().add(a_b);
+    DB.save(parent);
 
-    var planStart = new StrategicPlan();
-    planStart.setName("aaa");
-    planStart.setGoodsCapacities(new ArrayList<>());
-    DB.save(planStart);
-
-    var planFirstCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planFirstCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_b = new GoodsCapacity();
-    a_b.setGoods(g);
-    planFirstCapacity.getGoodsCapacities().add(a_b);
-    DB.save(planFirstCapacity);
-
-    var planSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-//    planSecondCapacity.setGoodsCapacities(new ArrayList<>());
-    planSecondCapacity.getGoodsCapacities().clear();
-    var a_c = new GoodsCapacity();
-    a_c.setGoods(g);
-    planSecondCapacity.getGoodsCapacities().add(a_c);
+    // act
+    var secondParent = DB.find(OmBeanListParent.class, parent.getId());
+    secondParent.getChildren().clear(); // <!-- HERE: Using clear() with orphanRemoval
+    var a_c = new OmBeanListChild("c");
+    secondParent.getChildren().add(a_c);
     // force version increase
-    DB.markAsDirty(planSecondCapacity);
-    DB.save(planSecondCapacity);
+    DB.markAsDirty(secondParent);
+    DB.save(secondParent);
 
-    var refreshedPlanSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().size()).isEqualTo(1);
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().get(0).getId()).isEqualTo(a_c.getId());
-    assertThat(refreshedPlanSecondCapacity.getVersion()).isGreaterThan(planFirstCapacity.getVersion());
-    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isAfter(planFirstCapacity.getWhenModified());
+    var refreshedParent = DB.find(OmBeanListParent.class, parent.getId());
+    assertThat(refreshedParent.getChildren().size()).isEqualTo(1);
+    assertThat(refreshedParent.getChildren().get(0).getId()).isEqualTo(a_c.getId());
+    assertThat(refreshedParent.getVersion()).isGreaterThan(parent.getVersion());
+    assertThat(refreshedParent.getWhenModified()).isAfter(parent.getWhenModified());
   }
 
   @Test
-  public void oneToManyShouldBeDeletedWithParentManualModifiedWhenTest() {
-    var g = new GoodsEntity();
-    DB.save(g);
+  void usingNewListWithParentDirty_expect_orphanDeleted_fails() {
+    // setup
+    var parent = new OmBeanListParent();
+    var a_b = new OmBeanListChild("b");
+    parent.getChildren().add(a_b);
+    DB.save(parent);
 
-    var planStart = new StrategicPlan();
-    planStart.setName("aaa");
-    planStart.setGoodsCapacities(new ArrayList<>());
-    DB.save(planStart);
+    // act
+    var secondParent = DB.find(OmBeanListParent.class, parent.getId());
+    secondParent.setChildren2(new ArrayList<>()); // <!-- HERE: new ArrayList with orphanRemoval
+    var a_c = new OmBeanListChild("c");
+    secondParent.getChildren().add(a_c);
+    secondParent.setWhenCreated(Instant.now());
+    DB.save(secondParent);
 
-    var planFirstCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planFirstCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_b = new GoodsCapacity();
-    a_b.setGoods(g);
-    planFirstCapacity.getGoodsCapacities().add(a_b);
-    DB.save(planFirstCapacity);
-
-    var planSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    planSecondCapacity.setGoodsCapacities(new ArrayList<>());
-    var a_c = new GoodsCapacity();
-    a_c.setGoods(g);
-    planSecondCapacity.getGoodsCapacities().add(a_c);
-    planSecondCapacity.setWhenCreated(new Timestamp(System.currentTimeMillis()));
-
-    DB.save(planSecondCapacity);
-
-    var refreshedPlanSecondCapacity = DB.find(StrategicPlan.class, planStart.getId());
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().size()).isEqualTo(1);
-    assertThat(refreshedPlanSecondCapacity.getGoodsCapacities().get(0).getId()).isEqualTo(a_c.getId());
-    assertThat(refreshedPlanSecondCapacity.getVersion()).isGreaterThan(planFirstCapacity.getVersion());
-    assertThat(refreshedPlanSecondCapacity.getWhenModified()).isAfter(planFirstCapacity.getWhenModified());
+    var refreshedParent = DB.find(OmBeanListParent.class, parent.getId());
+    assertThat(refreshedParent.getChildren().size()).isEqualTo(1);
+    assertThat(refreshedParent.getChildren().get(0).getId()).isEqualTo(a_c.getId());
+    assertThat(refreshedParent.getVersion()).isGreaterThan(parent.getVersion());
+    assertThat(refreshedParent.getWhenModified()).isAfter(parent.getWhenModified());
   }
 }
