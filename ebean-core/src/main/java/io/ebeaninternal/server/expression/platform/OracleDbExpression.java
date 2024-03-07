0816810d3b0718bc7df88da3d2b39d155455bbcc
Rob Bygrave <robin.bygrave@gmail.com>
2024-03-07 22:45:21 +1300

    Refactor extract DbExpressionRequest interface for db platform specific expressions
    This provides a simpler DbExpressionRequest interface for db platform specific expression adapters (rather than the SpiExpressionRequest which has more features that we don't wish to expose to those expression adapters.

--- ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/OracleDbExpression.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/expression/platform/OracleDbExpression.java
@@ -1,6 +1,5 @@
 package io.ebeaninternal.server.expression.platform;
 
-import io.ebeaninternal.api.SpiExpressionRequest;
 import io.ebeaninternal.server.expression.BitwiseOp;
 import io.ebeaninternal.server.expression.Op;
 
@@ -15,7 +14,7 @@ final class OracleDbExpression extends BaseDbExpression {
   }
 
   @Override
-  public void json(SpiExpressionRequest request, String propName, String path, Op operator, Object value) {
+  public void json(DbExpressionRequest request, String propName, String path, Op operator, Object value) {
     if (operator == Op.EXISTS) {
       request.append("json_exists(").property(propName).append(", '$.").append(path).append("')");
     } else if (operator == Op.NOT_EXISTS) {
@@ -26,17 +25,17 @@ final class OracleDbExpression extends BaseDbExpression {
   }
 
   @Override
-  public void bitwise(SpiExpressionRequest request, String propName, BitwiseOp operator, long flags, String compare, long match) {
+  public void bitwise(DbExpressionRequest request, String propName, BitwiseOp operator, long flags, String compare, long match) {
     bitwiseFunction(request, propName, operator, compare);
   }
 
   @Override
-  public void arrayContains(SpiExpressionRequest request, String propName, boolean contains, Object... values) {
+  public void arrayContains(DbExpressionRequest request, String propName, boolean contains, Object... values) {
     throw new IllegalStateException("ARRAY expressions not supported on Oracle");
   }
 
   @Override
-  public void arrayIsEmpty(SpiExpressionRequest request, String propName, boolean empty) {
+  public void arrayIsEmpty(DbExpressionRequest request, String propName, boolean empty) {
     throw new IllegalStateException("ARRAY expressions not supported on Oracle");
   }
 }
