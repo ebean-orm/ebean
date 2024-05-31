59daf5689a4770ffeec925a0cba9f4b42490972a
Rob Bygrave <robin.bygrave@gmail.com>
2024-05-31 21:52:38 +1200

    Fix logging message for naturalKey MPUT to include the natural key properties

--- ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorCacheHelp.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorCacheHelp.java
@@ -514,7 +514,7 @@ final class BeanDescriptorCacheHelp<T> {
 
     if (natKeys != null && !natKeys.isEmpty()) {
       if (natLog.isLoggable(DEBUG)) {
-        natLog.log(DEBUG, " MPUT {0}({1}, {2})", cacheName, naturalKey, natKeys.keySet());
+        natLog.log(DEBUG, " MPUT {0}({1}, {2})", cacheName, Arrays.toString(naturalKey), natKeys.keySet());
       }
       naturalKeyCache.putAll(natKeys);
     }
