a7fe5d2b8476297f7dad685ac4cc32c6fae5a79e
Rob Bygrave <robin.bygrave@gmail.com>
2024-09-02 19:28:11 +1200

    #3466 - Duplicated entities in findMany when a ManyToOne relation is fetched (Postgresql)
    Internally in OrmQueryDetail.markQueryJoins() the SpiQueryManyJoin is determined and returned. This change uses this and effectively removes the code from OrmQueryRequest.determineMany() and BeanDescriptor.manyProperty() which was the source of this issue (performing a similar task but not correctly taking the full path into account and hence the source of this bug).

---
+++ ebean-core/src/main/java/io/ebeaninternal/api/SpiQueryManyJoin.java
@@ -0,0 +1,17 @@
+package io.ebeaninternal.api;
+
+/**
+ * A SQL Join for a ToMany property included in the query.
+ */
+public interface SpiQueryManyJoin {
+
+  /**
+   * The full path of the many property to include in the query via join.
+   */
+  String path();
+
+  /**
+   * Order by clause defined via mapping on the ToMany property.
+   */
+  String fetchOrderBy();
+}
