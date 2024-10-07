538020bcb624b125c6f45b9bc0a59387b8cf6b94
Roland Praml <roland.praml@foconis.de>
2024-10-07 15:09:02 +0200

    DbJson Support for Dto-Queries

--- ebean-core/src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanProperty.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanProperty.java
@@ -36,7 +36,7 @@ import java.util.Set;
  * Description of a property of a bean. Includes its deployment information such
  * as database column mapping information.
  */
-public class DeployBeanProperty {
+public class DeployBeanProperty implements DeployProperty {
 
   private static final int ID_ORDER = 1000000;
   private static final int UNIDIRECTIONAL_ORDER = 100000;
@@ -223,6 +223,11 @@ public class DeployBeanProperty {
     return desc;
   }
 
+  @Override
+  public Class<?> getOwnerType() {
+    return desc.getBeanType();
+  }
+
   /**
    * Return the DB column length for character columns.
    * <p>
@@ -255,10 +260,12 @@ public class DeployBeanProperty {
     this.jsonDeserialize = jsonDeserialize;
   }
 
+  @Override
   public MutationDetection getMutationDetection() {
     return mutationDetection;
   }
 
+  @Override
   public void setMutationDetection(MutationDetection dirtyDetection) {
     this.mutationDetection = dirtyDetection;
   }
@@ -473,8 +480,9 @@ public class DeployBeanProperty {
   }
 
   /**
-   * Return true if this property is mandatory.
+   * Return true if this property is not mandatory.
    */
+  @Override
   public boolean isNullable() {
     return nullable;
   }
@@ -864,6 +872,7 @@ public class DeployBeanProperty {
   /**
    * Return the generic type for this property.
    */
+  @Override
   public Type getGenericType() {
     return genericType;
   }
@@ -1017,6 +1026,7 @@ public class DeployBeanProperty {
     return null;
   }
 
+  @Override
   @SuppressWarnings("unchecked")
   public <A extends Annotation> List<A> getMetaAnnotations(Class<A> annotationType) {
     List<A> result = new ArrayList<>();
