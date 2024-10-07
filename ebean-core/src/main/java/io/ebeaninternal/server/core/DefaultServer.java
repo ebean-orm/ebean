a67388a34d38e4f0701461ffd4b484eed6e2f80b
Roland Praml <roland.praml@foconis.de>
2024-10-07 15:13:43 +0200

    DDL generation can be invoked separately

--- ebean-core/src/main/java/io/ebeaninternal/server/core/DefaultServer.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/core/DefaultServer.java
@@ -2322,4 +2322,9 @@ public final class DefaultServer implements SpiServer, SpiEbeanServer {
   List<MetaQueryPlan> queryPlanCollectNow(QueryPlanRequest request) {
     return queryPlanManager.collect(request);
   }
+
+  @Override
+  public void runDdl() {
+    ddlGenerator.runDdl();
+  }
 }
