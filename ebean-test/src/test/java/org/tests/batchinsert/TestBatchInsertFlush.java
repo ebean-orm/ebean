a8418a3d75a5540b8cf9e9017e8f5b284f43315e
Roland Praml <roland.praml@foconis.de>
2024-10-08 13:16:32 +0200

    Created test for batch escalation

--- ebean-test/src/test/java/org/tests/batchinsert/TestBatchInsertFlush.java
+++ ebean-test/src/test/java/org/tests/batchinsert/TestBatchInsertFlush.java
@@ -15,6 +15,7 @@ import io.ebean.xtest.base.DtoQuery2Test;
 import io.ebeaninternal.api.SpiTransaction;
 import org.junit.jupiter.api.Test;
 import org.tests.model.basic.*;
+import org.tests.query.cache.Acl;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -357,6 +358,24 @@ public class TestBatchInsertFlush extends BaseTestCase {
 
     } finally {
       txn.end();
+    }
+  }
+
+  @Test
+  public void testBatchEscalationInsert() {
+    try (Transaction txn = DB.beginTransaction()) {
+      assertThat(txn.isBatchMode()).isFalse();
+      DB.insertAll(List.of(new Acl("test")));
+      assertThat(txn.isBatchMode()).isFalse();
+    }
+  }
+
+  @Test
+  public void testBatchEscalationSave() {
+    try (Transaction txn = DB.beginTransaction()) {
+      assertThat(txn.isBatchMode()).isFalse();
+      DB.saveAll(List.of(new Acl("test")));
+      assertThat(txn.isBatchMode()).isFalse();
     }
   }
 }
