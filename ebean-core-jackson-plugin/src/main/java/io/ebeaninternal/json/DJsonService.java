7f55d40eac97bda9715332ef2de4ffacd7928db2
Rob Bygrave <robin.bygrave@gmail.com>
2025-07-11 20:31:24 +1200

    Restore format

--- ebean-core-jackson-plugin/src/main/java/io/ebeaninternal/json/DJsonService.java
+++ ebean-core-jackson-plugin/src/main/java/io/ebeaninternal/json/DJsonService.java
@@ -1,45 +1,50 @@
 package io.ebeaninternal.json;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-
 import io.ebean.service.SpiJsonService;
 
-/** Utility that converts between JSON content and simple java Maps/Lists. */
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.*;
+
+/**
+ * Utility that converts between JSON content and simple java Maps/Lists.
+ */
 public final class DJsonService implements SpiJsonService {
 
-  /** Write the nested Map/List as json. */
+  /**
+   * Write the nested Map/List as json.
+   */
   @Override
   public String write(Object object) throws IOException {
     return EJsonWriter.write(object);
   }
 
-  /** Write the nested Map/List as json to the writer. */
+  /**
+   * Write the nested Map/List as json to the writer.
+   */
   @Override
   public void write(Object object, Writer writer) throws IOException {
     EJsonWriter.write(object, writer);
   }
 
-  /** Write the nested Map/List as json to the jsonGenerator. */
+  /**
+   * Write the nested Map/List as json to the jsonGenerator.
+   */
   @Override
   public void write(Object object, JsonGenerator jsonGenerator) throws IOException {
     EJsonWriter.write(object, jsonGenerator);
   }
 
-  /** Write the collection as json array to the jsonGenerator. */
+  /**
+   * Write the collection as json array to the jsonGenerator.
+   */
   @Override
   public void writeCollection(Collection<Object> collection, JsonGenerator jsonGenerator)
-      throws IOException {
+    throws IOException {
     EJsonWriter.writeCollection(collection, jsonGenerator);
   }
 
@@ -52,25 +57,33 @@ public final class DJsonService implements SpiJsonService {
     return EJsonReader.parseObject(json, modifyAware);
   }
 
-  /** Parse the json and return as a Map. */
+  /**
+   * Parse the json and return as a Map.
+   */
   @Override
   public Map<String, Object> parseObject(String json) throws IOException {
     return EJsonReader.parseObject(json);
   }
 
-  /** Parse the json and return as a Map taking a reader. */
+  /**
+   * Parse the json and return as a Map taking a reader.
+   */
   @Override
   public Map<String, Object> parseObject(Reader reader, boolean modifyAware) throws IOException {
     return EJsonReader.parseObject(reader, modifyAware);
   }
 
-  /** Parse the json and return as a Map taking a reader. */
+  /**
+   * Parse the json and return as a Map taking a reader.
+   */
   @Override
   public Map<String, Object> parseObject(Reader reader) throws IOException {
     return EJsonReader.parseObject(reader);
   }
 
-  /** Parse the json and return as a Map taking a JsonParser. */
+  /**
+   * Parse the json and return as a Map taking a JsonParser.
+   */
   @Override
   public Map<String, Object> parseObject(JsonParser parser) throws IOException {
     return EJsonReader.parseObject(parser);
@@ -86,56 +99,74 @@ public final class DJsonService implements SpiJsonService {
     return EJsonReader.parseObject(parser, token);
   }
 
-  /** Parse the json and return as a modify aware List. */
+  /**
+   * Parse the json and return as a modify aware List.
+   */
   @Override
   public <T> List<T> parseList(String json, boolean modifyAware) throws IOException {
     return EJsonReader.parseList(json, modifyAware);
   }
 
-  /** Parse the json and return as a List. */
+  /**
+   * Parse the json and return as a List.
+   */
   @Override
   public List<Object> parseList(String json) throws IOException {
     return EJsonReader.parseList(json);
   }
 
-  /** Parse the json and return as a List taking a Reader. */
+  /**
+   * Parse the json and return as a List taking a Reader.
+   */
   @Override
   public List<Object> parseList(Reader reader) throws IOException {
     return EJsonReader.parseList(reader);
   }
 
-  /** Parse the json and return as a List taking a JsonParser. */
+  /**
+   * Parse the json and return as a List taking a JsonParser.
+   */
   @Override
   public List<Object> parseList(JsonParser parser) throws IOException {
     return EJsonReader.parseList(parser, false);
   }
 
-  /** Parse the json returning as a List taking into account the current token. */
+  /**
+   * Parse the json returning as a List taking into account the current token.
+   */
   @Override
   @SuppressWarnings("unchecked")
   public <T> List<T> parseList(JsonParser parser, JsonToken currentToken) throws IOException {
     return (List<T>) EJsonReader.parse(parser, currentToken, false);
   }
 
-  /** Parse the json and return as a List or Map. */
+  /**
+   * Parse the json and return as a List or Map.
+   */
   @Override
   public Object parse(String json) throws IOException {
     return EJsonReader.parse(json);
   }
 
-  /** Parse the json and return as a List or Map. */
+  /**
+   * Parse the json and return as a List or Map.
+   */
   @Override
   public Object parse(Reader reader) throws IOException {
     return EJsonReader.parse(reader);
   }
 
-  /** Parse the json and return as a List or Map. */
+  /**
+   * Parse the json and return as a List or Map.
+   */
   @Override
   public Object parse(JsonParser parser) throws IOException {
     return EJsonReader.parse(parser);
   }
 
-  /** Parse the json returning a Set that might be modify aware. */
+  /**
+   * Parse the json returning a Set that might be modify aware.
+   */
   @Override
   public <T> Set<T> parseSet(String json, boolean modifyAware) throws IOException {
     List<T> list = parseList(json, modifyAware);
@@ -150,7 +181,9 @@ public final class DJsonService implements SpiJsonService {
     }
   }
 
-  /** Parse the json returning as a Set taking into account the current token. */
+  /**
+   * Parse the json returning as a Set taking into account the current token.
+   */
   @Override
   public <T> Set<T> parseSet(JsonParser parser, JsonToken currentToken) throws IOException {
     return new LinkedHashSet<>(parseList(parser, currentToken));
