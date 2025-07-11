a04b889155cf20f93184ebb7af156c85bb9b3bef
Rob Bygrave <robin.bygrave@gmail.com>
2025-07-11 20:43:26 +1200

    Trim up dependencies

--- ebean-core-jackson-plugin/src/main/java/module-info.java
+++ ebean-core-jackson-plugin/src/main/java/module-info.java
@@ -2,10 +2,8 @@ module io.ebean.core.internal.jackson {
 
 requires io.ebean.api;
 
-  requires transitive com.fasterxml.jackson.databind;
+  requires transitive com.fasterxml.jackson.core;
   exports io.ebeaninternal.json to io.ebean.test,io.ebean.core;
 
-  provides io.ebean.service.BootstrapService with
-    io.ebeaninternal.json.DJsonService;
-
+  provides io.ebean.service.BootstrapService with io.ebeaninternal.json.DJsonService;
 }
