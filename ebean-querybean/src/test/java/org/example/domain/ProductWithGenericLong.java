e9a77fbeff6eadc34a4be118d87f3a4d0d049072
dferreira <diogo.ferreira@premium-minds.com>
2025-11-24 11:53:25 +0000

    adds tests for ebean-querybean

---
+++ ebean-querybean/src/test/java/org/example/domain/ProductWithGenericLong.java
@@ -0,0 +1,48 @@
+package org.example.domain;
+
+import org.example.domain.ProductWithGenericLong;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ * Product entity bean.
+ */
+@Entity
+@Table(name = "o_product", schema = "foo")
+public class ProductWithGenericLong extends GenericBaseModel<Long> {
+
+  @Size(max = 20)
+  String sku;
+
+  String name;
+
+  /**
+   * Return sku.
+   */
+  public String getSku() {
+    return sku;
+  }
+
+  /**
+   * Set sku.
+   */
+  public void setSku(String sku) {
+    this.sku = sku;
+  }
+
+  /**
+   * Return name.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Set name.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
