515b8e5958b079dcdff71511c3ccf26e3386bf04
Rob Bygrave <robin.bygrave@gmail.com>
2026-05-05 22:54:58 +1200

    Performance improvement in OrmQueryDetail with copyInto() (#3763)
    Relatively minor improvement from reusing existing OrmQueryDetail

--- ebean-core/src/test/java/io/ebeaninternal/server/querydefn/OrmQueryPropertiesTest.java
+++ ebean-core/src/test/java/io/ebeaninternal/server/querydefn/OrmQueryPropertiesTest.java
@@ -1,7 +1,9 @@
 package io.ebeaninternal.server.querydefn;
 
+import io.ebeaninternal.api.SpiExpressionList;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
 import java.util.LinkedHashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -100,6 +102,62 @@ public class OrmQueryPropertiesTest {
 
     OrmQueryProperties p1 = new OrmQueryProperties("customer", "id,name,+cache");
     //FIXME: assertThat(append("fetch ", p1)).isEqualTo("fetch customer (id,name,+cache)");
+  }
+
+  @Test
+  public void queryPlanHash_expect_stableAndIncludeDynamicParts() {
+
+    OrmQueryProperties p1 = new OrmQueryProperties("customer", "id,name");
+
+    String hash1 = queryPlanHash(p1);
+    String hash2 = queryPlanHash(p1);
+    assertThat(hash2).isEqualTo(hash1);
+
+    p1.addSecondaryQueryJoin("id");
+    String hash3 = queryPlanHash(p1);
+    assertThat(hash3).isNotEqualTo(hash1);
+    assertThat(hash3).contains("/s[id]");
+
+    p1.setFilterMany(dummyFilterMany("f1"));
+    String hash4 = queryPlanHash(p1);
+    assertThat(hash4).contains("/f");
+    assertThat(hash4).contains("f1");
+  }
+
+  private String queryPlanHash(OrmQueryProperties properties) {
+    StringBuilder sb = new StringBuilder();
+    properties.queryPlanHash(sb);
+    return sb.toString();
+  }
+
+  private SpiExpressionList<?> dummyFilterMany(String marker) {
+    return (SpiExpressionList<?>) Proxy.newProxyInstance(
+      getClass().getClassLoader(),
+      new Class<?>[]{SpiExpressionList.class},
+      (proxy, method, args) -> {
+        String methodName = method.getName();
+        if ("queryPlanHash".equals(methodName)) {
+          ((StringBuilder) args[0]).append(marker);
+          return null;
+        }
+        Class<?> returnType = method.getReturnType();
+        if (returnType == boolean.class) {
+          return false;
+        }
+        if (returnType == int.class) {
+          return 0;
+        }
+        if (returnType == long.class) {
+          return 0L;
+        }
+        if (returnType == float.class) {
+          return 0f;
+        }
+        if (returnType == double.class) {
+          return 0d;
+        }
+        return null;
+      });
   }
 
 }
