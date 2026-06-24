274411b8fab3b496025a137cea2328a1f2f9aba1
robin.bygrave <robin.bygrave@eroad.com>
2026-06-24 18:23:17 +1200

    Add some missing @Override annotations

--- ebean-core/src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanProperty.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/deploy/meta/DeployBeanProperty.java
@@ -439,6 +439,7 @@ public class DeployBeanProperty implements DeployProperty {
   /**
    * Return the name of the property.
    */
+  @Override
   public String getName() {
     return name;
   }
@@ -872,6 +873,7 @@ public class DeployBeanProperty implements DeployProperty {
   /**
    * Return the property type.
    */
+  @Override
   public Class<?> getPropertyType() {
     return propertyType;
   }
